c43091a8877e135cd38e5788bab4d16e650181c2
Cameron Kingsbury <camsbury7@gmail.com>
2018-05-22 08:29:02 -0400

    feat: update initialization

--- cmacs/init.el
+++ cmacs/init.el
@@ -1,7 +1,10 @@
 ;; initialize installed packages for configuration
 (package-initialize)
 
- (setq load-path
-       (cons "~/.emacs.d/config" load-path))
+(setq load-path
+      (cons "~/.emacs.d/config" load-path))
+
+(setq initial-buffer-choice "*scratch*")
+;; (setq initial-major-mode 'emacs-lisp-mode) - why doesn't this work
 
 (require 'config)
