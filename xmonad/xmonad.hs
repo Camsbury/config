33383e3393e79eca7ccdd91edd4f6ebcbd533c58
Cameron Kingsbury <camsbury7@gmail.com>
2018-05-26 17:10:39 -0400

    feat: select app with hotkey

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,9 +1,9 @@
 import Control.Monad
 import Data.Bool
+import Data.Foldable
 import Data.Traversable
 import System.IO
 import XMonad
-import XMonad.Actions.WindowBringer
 import XMonad.Layout
 import XMonad.Layout.NoBorders
 import XMonad.Util.EZConfig
@@ -16,31 +16,24 @@ main = do
          , focusFollowsMouse = False
          } `additionalKeysP` myKeys
 
-myKeys = [ ("M1-M4-S-C-c", grabApp "chromium")
-         , ("M1-M4-S-C-e", grabApp "emacs")
+myKeys = [ ("M1-M4-S-C-c", grabApp "chromium" "Chromium-browser")
+         , ("M1-M4-S-C-e", grabApp "emacs" "Emacs")
+         , ("M1-M4-S-C-t", grabApp "xterm" "XTerm")
          ]
 
--- .StackSet -> W.allWindows :: Eq a => StackSet i l a s sd -> [a]
--- .Core -> withWindowSet :: (WindowSet -> X a) -> X a
--- .Core -> withDisplay :: (Display -> X a) -> X a
--- resClass/getClassHint?
 findAppWindows :: String -> X [Window]
 findAppWindows queriedName = withWindowSet $ \windowSet -> do
   let windowsV = W.allWindows windowSet
   filterM (hasWindowName queriedName) windowsV
-      -- (\window -> do
-      --   windowName <- withDisplay $ \display -> fmap resClass . liftIO $ getClassHint display window
-      --   pure $ bool [] [window] (windowName == name) :: X [Window]
-      -- ) >>= pure . join
 
 hasWindowName :: String -> Window -> X Bool
 hasWindowName queriedName window = do
-  windowName <- withDisplay $ \display -> fmap resClass . liftIO $ getClassHint display window
+  windowName <- runQuery className window
   pure $ windowName == queriedName
 
-grabApp :: String -> X ()
-grabApp app = do
-  winV <- findAppWindows app
+grabApp :: String -> String -> X ()
+grabApp spawnName matchName = do
+  winV <- findAppWindows matchName
   if length winV > 0 then
     windows . W.focusWindow $ head winV
-    else spawn app
+    else spawn spawnName
