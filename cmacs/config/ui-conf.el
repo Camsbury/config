5a2714894d52c8d39de8a6281ca62a39cb4ab7fc
Cameron Kingsbury <camsbury7@gmail.com>
2018-05-28 20:43:11 -0400

    feat: add smooth scroll to emacs

--- cmacs/config/ui-conf.el
+++ cmacs/config/ui-conf.el
@@ -11,4 +11,11 @@
 (rainbow-identifiers-mode)
 (load-theme 'doom-molokai t)
 
+;; scroll settings
+(setq redisplay-dont-pause t
+      scroll-margin 1
+      scroll-step 1
+      scroll-conservatively 10000
+      scroll-preserve-screen-position 1)
+
 (provide 'ui-conf)
