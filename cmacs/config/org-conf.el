499a72f8f94d8c058685f1dbbb48d2194f30413a
Cameron Kingsbury <camsbury7@gmail.com>
2018-06-02 16:05:02 -0400

    refactor: reorganize org

--- cmacs/config/org-conf.el
+++ cmacs/config/org-conf.el
@@ -1,10 +1,11 @@
 (require 'bindings-conf)
 
 (setq org-directory (expand-file-name "~/projects/lxndr/")
-      org-capture-templates '(("n" "Push onto Task Stack" entry
-                               (file+headline "~/projects/lxndr/task_stack.org" "Tasks") "* [ ] %i%?"))
-      org-agenda-files '("~projects/lxndr/queue.org")
+      org-capture-templates '(("n" "Place in the Inbox" entry
+                               (file+headline "~/projects/lxndr/inbox.org" "Inbox") "* [ ] %i%?"))
+      org-agenda-files '("~/projects/lxndr/queue.org" "~/projects/lxndr/store.org")
       org-refile-targets '(("~/projects/lxndr/queue.org" :maxlevel . 3)
+                           ("~/projects/lxndr/store.org" :level . 1)
                            ("~/projects/lxndr/ref.org" :level . 1))
       org-archive-location (concat "~/projects/lxndr/archive/" (format-time-string "%Y-%m") ".org::")
       org-todo-keywords '((sequence "[ ]" "[x]")))
@@ -15,13 +16,18 @@
   [remap evil-quit]                    'org-capture-kill)
 
 (general-emacs-define-key org-mode-map
-  [remap evil-ret]                     'org-todo)
+  [remap evil-ret]          'org-todo
+  [remap org-return-indent] 'evil-window-down
+  "M-t"                     'org-todo
+  )
 
 (my-mode-leader-def
  :states  'normal
  :keymaps 'org-mode-map
  "a" 'org-archive-subtree
+ "d" 'org-deadline
  "r" 'org-refile
+ "t" 'org-set-tags-command
  )
 
 (general-add-hook 'org-mode-hook
