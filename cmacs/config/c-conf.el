ed29fbfc6311d5abc3167cd70acd549532a88059
Cameron Kingsbury <camsbury7@gmail.com>
2018-06-09 22:35:50 -0400

    refactor: set indent in C to 2

--- cmacs/config/c-conf.el
+++ cmacs/config/c-conf.el
@@ -1,12 +1,12 @@
-(setq c-basic-offset 4)
-(setq c-basic-indent 4)
+(setq c-basic-offset 2)
+(setq c-basic-indent 2)
 (general-add-hook 'c-mode-hook
-                  (list 'irony-mode
-                        'flycheck-mode
-                        'flycheck-irony-setup
-                        'irony-cdb-autosetup-compile-options
-                        'eldoc-mode
-                        'irony-eldoc
-                        'rainbow-delimiters-mode))
+  (list 'irony-mode
+        'flycheck-mode
+        'flycheck-irony-setup
+        'irony-cdb-autosetup-compile-options
+        'eldoc-mode
+        'irony-eldoc
+        'rainbow-delimiters-mode))
 
 (provide 'c-conf)
