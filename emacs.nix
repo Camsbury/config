fbcbfcc78db9a2a9aa4afe315ff1806442542950
Cameron Kingsbury <camsbury7@gmail.com>
2018-06-09 21:01:27 -0400

    feat: upgrade to emacs 26

--- emacs.nix
+++ emacs.nix
@@ -3,7 +3,15 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  camEmacs = pkgs.emacs;
+  # camEmacs = pkgs.emacs;
+  camEmacs = pkgs.stdenv.lib.overrideDerivation pkgs.emacs (
+    oldAttrs : {
+    version = "26.1";
+    src = pkgs.fetchurl {
+    url = "https://ftp.gnu.org/pub/gnu/emacs/emacs-26.1.tar.xz";
+    sha256 = "0b6k1wq44rc8gkvxhi1bbjxbz3cwg29qbq8mklq2az6p1hjgrx0w";
+
+    }; patches = [];});
   emacsWithPackages = (pkgs.emacsPackagesNgGen camEmacs).emacsWithPackages;
 in
   emacsWithPackages (epkgs: (with epkgs.melpaStablePackages; [
