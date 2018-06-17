29d55e9de5f342d02947043a5e6d1f0085bd8f90
Cameron Kingsbury <camsbury7@gmail.com>
2018-06-17 00:08:37 -0400

    fix: call evil after setting the hooks

--- cmacs/config/evil-conf.el
+++ cmacs/config/evil-conf.el
@@ -1,4 +1,3 @@
-(evil-mode)
 (general-add-hook 'evil-mode-hook
                   (list 'evil-surround-mode
                         'evil-commentary-mode
@@ -6,5 +5,7 @@
 
 ;; Yanks to end instead of whole line
 (setq evil-want-Y-yank-to-eol t)
+
+(evil-mode)
 
 (provide 'evil-conf)
