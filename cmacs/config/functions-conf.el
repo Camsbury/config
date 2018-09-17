199a83851a83b26001b1d58addd1518ac920888e
Cameron Kingsbury <camsbury7@gmail.com>
2018-09-16 20:49:25 -0400

    feat: add increment number at point

--- cmacs/config/functions-conf.el
+++ cmacs/config/functions-conf.el
@@ -100,4 +100,12 @@
   (spawn-right)
   (find-file "~/projects/nix_dots/emacs.nix"))
 
+;; Utility Functions
+(defun increment-number-at-point ()
+  (interactive)
+  (skip-chars-backward "0-9")
+  (or (looking-at "[0-9]+")
+      (error "No number at point"))
+  (replace-match (number-to-string (1+ (string-to-number (match-string 0))))))
+
 (provide 'functions-conf)
