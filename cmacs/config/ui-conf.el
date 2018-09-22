ba1e188ee12a262950ddca41929cb168dcc7fcf4
Camsbury <camsbury7@gmail.com>
2018-09-22 10:22:14 -0400

    feat: highlight keyword faces

--- cmacs/config/ui-conf.el
+++ cmacs/config/ui-conf.el
@@ -4,4 +4,11 @@
 (global-linum-mode)
 (global-auto-revert-mode)
 
+(setq hl-todo-keyword-faces
+      `(("TODO"  . ,(face-foreground 'warning))
+        ("FIXME" . ,(face-foreground 'error))
+        ("NOTE"  . ,(face-foreground 'success))))
+(general-add-hook 'prog-mode-hook
+  (list 'hl-todo-mode))
+
 (provide 'ui-conf)
