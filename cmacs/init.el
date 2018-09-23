1b36cae9a2b5199bdde66730e223511c3db99aaf
Cameron Kingsbury <camsbury7@gmail.com>
2018-09-23 18:12:15 -0400

    fix: choose os init in init.el

--- cmacs/init.el
+++ cmacs/init.el
@@ -4,13 +4,16 @@
 (setq load-path
       (cons "~/.emacs.d/config" load-path))
 (when (memq window-system '(ns))
-  (exec-path-from-shell-initialize))
+  (exec-path-from-shell-initialize)
+  (setq mac-command-modifier 'meta
+        mac-option-modifier 'super))
+(when (memq window-system '(x))
+  (keychain-refresh-environment))
 
 (setq initial-buffer-choice t)
 (setq auto-window-vscroll nil)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(keychain-refresh-environment)
 
 (add-hook 'after-init-hook (lambda () (require 'config)))
