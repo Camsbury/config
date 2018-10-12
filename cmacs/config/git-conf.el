cbfc2d2eb24903c15ab4fdc7133be25c77f7e941
Cameron Kingsbury <camsbury7@gmail.com>
2018-10-12 13:40:11 -0400

    feat: add quit for magit-blame

--- cmacs/config/git-conf.el
+++ cmacs/config/git-conf.el
@@ -1,3 +1,10 @@
 (general-add-hook 'magit-mode-hook
                   (list 'evil-magit-init))
+
+
+(general-define-key
+ :states  'normal
+ :keymaps 'magit-blame-mode-map
+ "q" 'magit-blame-quit)
+
 (provide 'git-conf)
