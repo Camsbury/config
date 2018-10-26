9f6a6fae9daf620d0874a6281f3ec6fde0fa6d35
Cameron Kingsbury <camsbury7@gmail.com>
2018-10-26 09:49:28 -0400

    feat: add git timemachine hotkeys

--- cmacs/config/git-conf.el
+++ cmacs/config/git-conf.el
@@ -5,6 +5,13 @@
 (general-define-key
  :states  'normal
  :keymaps 'magit-blame-mode-map
- "q" 'magit-blame-quit)
+ "q" #'magit-blame-quit)
+
+(general-define-key
+ :keymaps 'git-timemachine-mode-map
+ [remap evil-record-macro] #'git-timemachine-quit
+ [remap evil-window-up] #'git-timemachine-show-previous-revision
+ [remap evil-window-down] #'git-timemachine-show-next-revision)
+
 
 (provide 'git-conf)
