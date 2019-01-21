4e3ee685cba832bafc681c390a96143818ddd4a3
Cameron Kingsbury <camsbury7@gmail.com>
2019-01-21 12:05:50 -0500

    fix: projectile prefix add

--- cmacs/config/project-conf.el
+++ cmacs/config/project-conf.el
@@ -1,4 +1,7 @@
 (projectile-discover-projects-in-directory "~/projects")
 (setq projectile-globally-ignored-file-suffixes '("~" "#"))
 
+;; required to use counsel-projectile
+(setq projectile-keymap-prefix (kbd "C-c C-p"))
+
 (provide 'project-conf)
