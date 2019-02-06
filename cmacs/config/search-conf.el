be9b3b5193e1fe033aed1f251b2400373f886708
Cameron Kingsbury <camsbury7@gmail.com>
2019-02-06 14:57:18 -0500

    feat: improve wgrep save and quit

--- cmacs/config/search-conf.el
+++ cmacs/config/search-conf.el
@@ -1,4 +1,14 @@
 (require 'wgrep)
 (require 'avy)
 
+(defun wgrep-save-and-quit ()
+  "things"
+  (interactive)
+  (wgrep-finish-edit)
+  (wgrep-save-all-buffers)
+  (quit-window))
+
+(general-define-key :keymaps 'wgrep-mode-map
+  [remap evil-save-modified-and-close] #'wgrep-save-and-quit)
+
 (provide 'search-conf)
