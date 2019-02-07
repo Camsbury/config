255bc2ce512dbaeb0211946ca71e9460afe6d584
Cameron Kingsbury <camsbury7@gmail.com>
2019-02-07 09:11:49 -0500

    fix: update magit to use leader hydra

--- cmacs/config/git-conf.el
+++ cmacs/config/git-conf.el
@@ -10,29 +10,31 @@
 (general-define-key
  :states  'normal
  :keymaps 'magit-status-mode-map
- [remap magit-section-backward] #'evil-window-up
- [remap magit-section-forward] #'evil-window-down)
+ [remap magit-section-backward]  #'evil-window-up
+ [remap magit-section-forward]   #'evil-window-down
+ [remap indent-new-comment-line] #'magit-section-forward
+ [remap kill-sentence]           #'magit-section-backward)
 
 (general-define-key
  :keymaps 'git-timemachine-mode-map
  [remap evil-record-macro] #'git-timemachine-quit
- [remap evil-window-up] #'git-timemachine-show-previous-revision
- [remap evil-window-down] #'git-timemachine-show-next-revision)
+ [remap evil-window-up]    #'git-timemachine-show-previous-revision
+ [remap evil-window-down]  #'git-timemachine-show-next-revision)
 
 (general-define-key
  :states  'normal
  :keymaps 'magit-diff-mode-map
- "<RET>" #'magit-diff-visit-file-other-window
- "zz"  #'evil-scroll-line-to-center
- "zt"  #'evil-scroll-line-to-top
- "zb"  #'evil-scroll-line-to-bottom
- "L"   #'evil-window-bottom
- "H"   #'evil-window-top
- [remap magit-section-backward] #'evil-window-up
- [remap magit-section-forward] #'evil-window-down
+ "<RET>"                         #'magit-diff-visit-file-other-window
+ "zz"                            #'evil-scroll-line-to-center
+ "zt"                            #'evil-scroll-line-to-top
+ "zb"                            #'evil-scroll-line-to-bottom
+ "L"                             #'evil-window-bottom
+ "H"                             #'evil-window-top
+ [remap magit-section-backward]  #'evil-window-up
+ [remap magit-section-forward]   #'evil-window-down
  [remap indent-new-comment-line] #'magit-section-forward
- [remap kill-sentence] #'magit-section-backward
- [remap scroll-up] 'ignore
+ [remap kill-sentence]           #'magit-section-backward
+ [remap scroll-up]               #'hydra-leader-body
  )
 
 
