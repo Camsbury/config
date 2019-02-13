743c07e39f6f2e4afc43f3e566d225208bc10288
Cameron Kingsbury <camsbury7@gmail.com>
2019-02-13 09:29:50 -0500

    fix: smaller font for linux comp

--- cmacs/config/theme-conf.el
+++ cmacs/config/theme-conf.el
@@ -1,4 +1,7 @@
 (load-theme 'doom-molokai t)
-(set-default-font "DejaVu Sans Mono 8")
+(if (string-equal system-type "gnu/linux")
+    (set-default-font "DejaVu Sans Mono 6"))
+(if (string-equal system-type "darwin")
+    (set-default-font "DejaVu Sans Mono 8"))
 
 (provide 'theme-conf)
