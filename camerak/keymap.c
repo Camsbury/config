37a52cf674c34073293905661a7570f01d52ee49
Cameron Kingsbury <camsbury7@gmail.com>
2019-02-21 06:02:01 -0500

    feat: add camerak

---
+++ camerak/keymap.c
@@ -0,0 +1,356 @@
+#include "ergodox_ez.h"
+#include "debug.h"
+#include "action_layer.h"
+#include "version.h"
+
+
+#include "keymap_german.h"
+
+#include "keymap_nordic.h"
+
+
+
+#define CHROME_INSPECT LGUI(LSFT(KC_C))
+
+enum custom_keycodes {
+  PLACEHOLDER    = SAFE_RANGE, // can always be here
+  EPRM,
+  VRSN,
+  RGB_SLD,
+  QMK_LAYOUT,
+  EMACS_BINDINGS,
+  EMACS_CONFIG,
+  ZSHRC,
+  KHDRC,
+  CHUNKRC,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  // MAIN LAYER
+  [0] = KEYMAP(
+      //LEFT HAND
+      KC_NO,             KC_PGDOWN,      KC_PGUP,           KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, KC_MEDIA_PLAY_PAUSE, LCTL(KC_Q),
+      KC_TAB,            KC_Q,           KC_W,              KC_F,                KC_P,                KC_G,                LGUI(KC_X),
+      LT(1,KC_LBRACKET), KC_A,           KC_R,              KC_S,                KC_T,                KC_D,
+      KC_LSPO,           KC_Z,           KC_X,              KC_C,                KC_V,                KC_B,                KC_CAPSLOCK,
+      MEH_T(KC_NO),      ALL_T(KC_NO),   LSFT(LALT(KC_NO)), KC_LALT,             KC_LGUI,
+
+                                                                                 KC_NO,               KC_NO,
+                                                                                                                           KC_NO,
+                                                                                 KC_SPACE,            LT(2,KC_ESCAPE),     CTL_T(KC_NO)         ,
+
+
+      //RIGHT HAND
+      KC_NO,             KC_AUDIO_MUTE,  KC_AUDIO_VOL_UP,   KC_AUDIO_VOL_DOWN,   LGUI(KC_KP_PLUS),    LGUI(KC_MINUS),      KC_POWER,
+      LGUI(KC_SPACE),    KC_J,           KC_L,              KC_U,                KC_Y,                KC_COLN,             KC_DELETE,
+                         KC_H,           KC_N,              KC_E,                KC_I,                KC_O,                LT(1,KC_RBRACKET),
+      KC_NO,             KC_K,           KC_M,              KC_COMMA,            KC_DOT,              KC_SLASH,            KC_RSPC,
+                         KC_LGUI,        KC_LALT,           LSFT(LALT(KC_NO)),   ALL_T(KC_NO),        MEH_T(KC_NO),
+
+      KC_NO,
+                         KC_NO,          KC_NO,
+      CTL_T(KC_NO),      LT(2,KC_ENTER), KC_BSPACE),
+
+  // SYMBOL LAYER
+  [1] = KEYMAP(
+      //LEFT HAND
+      KC_NO,   KC_NO,      KC_NO,          KC_NO,          KC_NO,          KC_NO,     KC_NO,
+      KC_NO,   KC_DLR,     KC_CIRC,        KC_HASH,        KC_UNDS,        KC_NO,     KC_NO,
+      KC_TRNS, KC_PLUS, KC_ASTERISK, KC_EQUAL,       KC_MINUS,       KC_QUES,
+      KC_LCBR, KC_LEFT,    KC_DOWN,        KC_UP,          KC_RIGHT,       KC_NO,     KC_NO,
+      KC_NO,   KC_NO,      KC_NO,          KC_NO,          KC_NO,
+
+                                                           KC_NO,          KC_NO,
+                                                                                      KC_NO,
+                                                           KC_NO,          KC_NO,     KC_NO,
+
+
+      //RIGHT HAND
+      KC_NO,   KC_NO,      KC_NO,          KC_NO,          KC_NO,          KC_NO,     KC_NO,
+      KC_NO,   KC_NO,      KC_AT,          KC_PERC,        KC_PIPE,        KC_SCOLON, KC_NO,
+               KC_EXLM,    KC_QUOTE,       KC_DQUO,        KC_GRAVE,       KC_TILD,   KC_TRNS,
+      KC_NO,   KC_NO,      KC_AMPR,        KC_LABK,        KC_RABK,        KC_BSLASH, KC_RCBR,
+                           KC_NO,          KC_NO,          KC_NO,          KC_NO,     KC_NO,
+
+      KC_NO,
+               KC_NO,      KC_NO,
+      KC_NO,   KC_NO,      KC_NO),
+
+  // NUMBER LAYER
+  [2] = KEYMAP(
+      //LEFT HAND
+      CHROME_INSPECT, KC_F1,        KC_F2,        KC_F3,      KC_F4,          KC_F5,        KC_NO,
+      KC_NO,          KC_NO,        KC_NO,        QMK_LAYOUT, EMACS_BINDINGS, KC_NO,        KC_NO,
+      KC_NO,          KC_1,         KC_2,         KC_3,       KC_4,           KC_5,
+      KC_LCTL,        KC_NO,        KC_NO,        KC_NO,      EMACS_CONFIG,   KC_NO,        KC_NO,
+      KC_NO,          ALL_T(KC_NO), MEH_T(KC_NO), KC_LALT,    KC_LGUI,
+
+                                                              KC_NO,          KC_NO,
+                                                                                            KC_NO,
+                                                              KC_NO,          KC_TRNS,      KC_NO,
+
+
+      //RIGHT HAND
+      KC_NO,          KC_F6,        KC_F7,        KC_F8,      KC_F9,          KC_F10,       KC_F11,
+      KC_NO,          KC_NO,        ZSHRC,        KC_NO,      KC_NO,          KC_NO,        KC_F12,
+                      KC_6,         KC_7,         KC_8,       KC_9,           KC_0,         KC_NO,
+      KC_NO,          KC_NO,        KHDRC,        CHUNKRC,    KC_NO,          KC_NO,        KC_LCTL,
+                                    KC_LGUI,      KC_LALT,    MEH_T(KC_NO),   ALL_T(KC_NO), KC_NO,
+
+      KC_NO,
+                      KC_NO,        KC_NO,
+      TO(3),          KC_TRNS,      TO(4)),
+
+  // SC2 LAYER
+  [3] = KEYMAP(
+      //BATTLE HAND
+      KC_ESCAPE,      KC_1,           KC_2,           KC_3,                KC_4,             KC_5,           KC_6,
+      KC_TAB,         KC_Q,           KC_W,           KC_F,                KC_P,             KC_G,           KC_HOME,
+      KC_LBRACKET,    KC_A,           KC_R,           KC_S,                KC_T,             KC_D,
+      KC_RBRACKET,    KC_Z,           KC_X,           KC_C,                KC_V,             KC_B,           KC_QUOTE,
+      MEH_T(KC_NO),   KC_7,           KC_8,           KC_9,                KC_0,
+
+                                                                           LCA_T(KC_NO),     C_S_T(KC_NO),
+                                                                                                             LSFT(LALT_T(KC_NO)),
+                                                                           LCTL_T(KC_NO),    LALT_T(KC_NO),  LSFT_T(KC_NO),
+
+
+      //AUXILIARY HAND
+      KC_NO,          KC_AUDIO_MUTE,  KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN,  LGUI(KC_PLUS), LGUI(KC_MINUS), TO(0),
+      LGUI(KC_SPACE), KC_J,           KC_L,            KC_U,               KC_Y,             KC_COLN,        KC_DELETE,
+                      KC_H,           KC_N,            KC_E,               KC_I,             KC_O,           KC_RBRACKET,
+      KC_NO,          KC_K,           KC_M,            KC_COMMA,           KC_DOT,           KC_SLASH,       KC_RSPC,
+                                      KC_LGUI,         KC_LALT,            MEH_T(KC_NO),     ALL_T(KC_NO),   KC_NO,
+
+      KC_NO,
+                      KC_NO,          KC_NO,
+      KC_NO,          KC_ENTER,       KC_BSPACE),
+
+  // WC3 LAYER
+  [4] = KEYMAP(
+      //BATTLE HAND
+      CTL_T(KC_NO),      KC_1,           KC_2,            KC_3,               KC_4,             KC_5,            KC_6,
+      KC_TAB,            KC_Q,           KC_W,            KC_F,               KC_P,             KC_G,            KC_7,
+      LT(1,KC_LBRACKET), KC_A,           KC_R,            KC_S,               KC_T,             KC_D,
+      KC_LSPO,           KC_Z,           KC_X,            KC_C,               KC_V,             KC_B,            KC_8,
+      KC_F1,             KC_F2,          KC_F3,           KC_LALT,            CTL_T(KC_NO),
+
+                                                                              KC_9,             KC_0,
+                                                                                                                 KC_NO,
+                                                                              KC_SPACE,         LT(5,KC_ESCAPE), KC_LSFT,
+
+
+      //AUXILIARY HAND
+      KC_NO,             KC_AUDIO_MUTE,  KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN,  LGUI(KC_PLUS), LGUI(KC_MINUS),  TO(0),
+      LGUI(KC_SPACE),    KC_J,           KC_L,            KC_U,               KC_Y,             KC_COLN,         KC_DELETE,
+                         KC_H,           KC_N,            KC_E,               KC_I,             KC_O,            LT(1,KC_RBRACKET),
+      KC_NO,             KC_K,           KC_M,            KC_COMMA,           KC_DOT,           KC_SLASH,        KC_RSPC,
+                                         KC_LGUI,         KC_LALT,            MEH_T(KC_NO),     ALL_T(KC_NO),    KC_NO,
+
+      KC_NO,
+                         KC_NO,          KC_NO,
+      CTL_T(KC_NO),      LT(2,KC_ENTER), KC_BSPACE),
+
+  // WC3 LAYER 2
+  [5] = KEYMAP(
+      //LEFT HAND
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_NO, KC_P7, KC_P8, KC_P9, KC_NO,
+      KC_NO, KC_NO, KC_NO, KC_P4, KC_P5, KC_P6,
+      KC_NO, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_NO,
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+
+                                  KC_NO, KC_NO,
+                                                KC_NO,
+                                  KC_NO, KC_NO, KC_NO,
+
+
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+
+      KC_NO,
+             KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_NO),
+
+  // NO LAYER
+  /* [#] = KEYMAP( */
+  /*     //LEFT HAND */
+  /*     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+  /*     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+  /*     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+  /*     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+  /*     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+
+  /*                                 KC_NO, KC_NO, */
+  /*                                               KC_NO, */
+  /*                                 KC_NO, KC_NO, KC_NO, */
+
+
+  /*     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+  /*     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+  /*            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+  /*     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+  /*                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+
+  /*     KC_NO, */
+  /*            KC_NO, KC_NO, */
+  /*     KC_NO, KC_NO, KC_NO, */
+
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+  [1] = ACTION_LAYER_TAP_TOGGLE(1)
+};
+
+// leaving this in place for compatibilty with old keymaps cloned and re-compiled.
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+      switch(id) {
+        case 0:
+        if (record->event.pressed) {
+          SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+        }
+        break;
+      }
+    return MACRO_NONE;
+};
+
+void matrix_init_user(void) {
+#ifdef RGBLIGHT_COLOR_LAYER_0
+  rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+#endif
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    // dynamically generate these.
+    case QMK_LAYOUT:
+      if (record->event.pressed) {
+          SEND_STRING("  ce");
+      }
+      return false;
+      break;
+    case EMACS_BINDINGS:
+      if (record->event.pressed) {
+          SEND_STRING("  cb");
+      }
+      return false;
+      break;
+    case EMACS_CONFIG:
+      if (record->event.pressed) {
+          SEND_STRING("  cc");
+      }
+      return false;
+      break;
+    case ZSHRC:
+      if (record->event.pressed) {
+          SEND_STRING("  cn");
+      }
+      return false;
+      break;
+    case KHDRC:
+      if (record->event.pressed) {
+          SEND_STRING("  ci");
+      }
+      return false;
+      break;
+    case CHUNKRC:
+      if (record->event.pressed) {
+          SEND_STRING("  cI");
+      }
+      return false;
+      break;
+    case EPRM:
+      if (record->event.pressed) {
+        eeconfig_init();
+      }
+      return false;
+      break;
+    case VRSN:
+      if (record->event.pressed) {
+        SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+      }
+      return false;
+      break;
+    case RGB_SLD:
+      if (record->event.pressed) {
+        rgblight_mode(1);
+      }
+      return false;
+      break;
+
+  }
+  return true;
+}
+
+uint32_t layer_state_set_user(uint32_t state) {
+
+    uint8_t layer = biton32(state);
+
+    ergodox_board_led_off();
+    ergodox_right_led_1_off();
+    ergodox_right_led_2_off();
+    ergodox_right_led_3_off();
+    switch (layer) {
+      case 0:
+        #ifdef RGBLIGHT_COLOR_LAYER_0
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+        #endif
+        break;
+      case 1:
+        ergodox_right_led_1_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_1
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_1);
+        #endif
+        break;
+      case 2:
+        ergodox_right_led_2_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_2
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_2);
+        #endif
+        break;
+      case 3:
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_3
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3);
+        #endif
+        break;
+      case 4:
+        ergodox_right_led_1_on();
+        ergodox_right_led_2_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_4
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_4);
+        #endif
+        break;
+      case 5:
+        ergodox_right_led_1_on();
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_5
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_5);
+        #endif
+        break;
+      case 6:
+        ergodox_right_led_2_on();
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_6
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
+        #endif
+        break;
+      case 7:
+        ergodox_right_led_1_on();
+        ergodox_right_led_2_on();
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_7
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
+        #endif
+        break;
+      default:
+        break;
+    }
+    return state;
+
+};
