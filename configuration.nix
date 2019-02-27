cbb620791d71c436c8fef3b73ce2547a9453ca4a
Cameron Kingsbury <camsbury7@gmail.com>
2019-02-26 21:33:26 -0500

    fix: detect windows with os prober

--- configuration.nix
+++ configuration.nix
@@ -23,6 +23,7 @@ in
     boot.loader.grub.enable = true;
     boot.loader.grub.version = 2;
     boot.loader.grub.device = "nodev";
+    boot.loader.grub.useOSProber = true;
     boot.loader.grub.efiSupport = true;
     boot.loader.efi.canTouchEfiVariables = true;
     boot.kernelPackages = pkgs.linuxPackages_latest;
