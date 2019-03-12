1403d558dfb9cbf2fc3f095454857c7c9b90f54b
Cameron Kingsbury <camsbury7@gmail.com>
2019-03-12 13:35:16 -0400

    feat: add back button for eww-mode

--- cmacs/config/browser-conf.el
+++ cmacs/config/browser-conf.el
@@ -1,4 +1,16 @@
+(require 'bindings-conf)
+
 (general-add-hook 'eww-mode-hook
-                  'visual-line-mode)
+                  'visual-line-mode
+                  (lambda () (call-interactively (buffer-face-set 'hl-line))))
+
+(general-def 'normal eww-mode-map
+ [remap empty-mode-leader] #'hydra-eww/body
+ )
+
+(defhydra hydra-eww (:exit t)
+  "eww-mode"
+  ("h" #'eww-back-url "back"))
+
 
 (provide 'browser-conf)
