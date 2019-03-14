a78f235b817346d0dd805fd441412ed02550b438
Cameron Kingsbury <camsbury7@gmail.com>
2019-03-14 14:03:10 -0400

    clean: move counsel-rg stuff to search-conf

--- cmacs/config/counsel-conf.el
+++ cmacs/config/counsel-conf.el
@@ -9,7 +9,5 @@
 (setq counsel-find-file-ignore-regexp "[~\#]$")
 (custom-set-faces
  '(ivy-current-match ((t (:background "#3a403a")))))
-(setq counsel-rg-base-command
-      "rg -S -g !.git --no-heading --line-number --hidden --color never %s .")
 
 (provide 'counsel-conf)
--- cmacs/config/search-conf.el
+++ cmacs/config/search-conf.el
@@ -8,6 +8,8 @@
   (wgrep-save-all-buffers)
   (quit-window))
 
+(setq counsel-rg-base-command
+      "rg -S -g !.git --no-heading --line-number --hidden --color never %s .")
 (general-define-key :keymaps 'wgrep-mode-map
   [remap evil-save-modified-and-close] #'wgrep-save-and-quit)
 
