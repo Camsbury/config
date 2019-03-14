0a7d9cca62a3d9b391a26e64a0930624d78a19d0
Cameron Kingsbury <camsbury7@gmail.com>
2019-03-14 09:10:16 -0400

    feat: info mode bindings in docs

--- cmacs/config/docs-conf.el
+++ cmacs/config/docs-conf.el
@@ -1,3 +1,16 @@
+(require 'hydra)
+(require 'bindings-conf)
 (setq helm-dash-common-docsets '("C" "Emacs Lisp" "Haskell" "HTML" "JavaScript" "Python 3" "React" "Rust"))
+
+
+(general-def Info-mode-map
+  [remap Info-history] 'ignore
+  )
+
+(general-def 'normal Info-mode-map
+  [remap Info-scroll-up] #'hydra-leader/body
+  [remap Info-history]   #'evil-window-bottom
+  "gg"                   #'evil-goto-first-line
+  )
 
 (provide 'docs-conf)
