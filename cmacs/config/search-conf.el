962f83d48741289e899a8a4495aab9c29de6f436
Cameron Kingsbury <camsbury7@gmail.com>
2019-03-22 09:49:19 -0400

    feat: remove silly things from counsel-rg search space

--- cmacs/config/search-conf.el
+++ cmacs/config/search-conf.el
@@ -9,7 +9,7 @@
   (quit-window))
 
 (setq counsel-rg-base-command
-      "rg -S -g !.git --no-heading --line-number --hidden --color never %s .")
+      "rg -S -g !.git -g !node_modules -g !yarn -g !lock --no-heading --line-number --hidden --color never %s .")
 (general-define-key :keymaps 'wgrep-mode-map
   [remap evil-save-modified-and-close] #'wgrep-save-and-quit)
 
