83212e77f993a5cd28c943fcb80ad3a0eb74d8ff
Cameron Kingsbury <camsbury7@gmail.com>
2019-07-01 23:36:57 -0400

    feat: help me see on my little laptop

--- cmacs/config/theme-conf.el
+++ cmacs/config/theme-conf.el
@@ -1,6 +1,8 @@
 (load-theme 'doom-molokai t)
 (if (string-equal system-type "gnu/linux")
-    (set-default-font "Roboto Mono 6"))
+    (if (> (x-display-pixel-width) 1600)
+        (set-default-font "Roboto Mono 6")
+      (set-default-font "Roboto Mono 8")))
 (if (string-equal system-type "darwin")
     (set-default-font "DejaVu Sans Mono 8"))
 
