1c6f915fdb79fc26c0f50c6f813f7e1f4ecc9dcd
Cameron Kingsbury <camsbury7@gmail.com>
2019-07-07 12:17:09 -0400

    feat: use `fetchFromGitHub` for pinning

--- unstable.nix
+++ unstable.nix
@@ -1,6 +1,15 @@
-builtins.fetchTarball {
-  name = "nixpkgs-unstable";
-  url = https://github.com/nixos/nixpkgs/archive/20b993ef2c9e818a636582ade9597f71a485209d.tar.gz;
-  sha256 = "0plb93vw662lwxpfac7karsqkkajwr6xp5pfgij2l67bn1if3xd6";
-}
-# builtins.fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz
+let
+  inherit (import <nixpkgs> {}) fetchFromGitHub;
+in
+  fetchFromGitHub {
+    owner = "NixOS";
+    repo  = "nixpkgs-channels";
+    rev    = "20b993ef2c9e818a636582ade9597f71a485209d";
+    sha256 = "0plb93vw662lwxpfac7karsqkkajwr6xp5pfgij2l67bn1if3xd6";
+  }
+  # fetchFromGitHub {
+  #   owner = "NixOS";
+  #   repo  = "nixpkgs-channels";
+  #   rev    = "nixos-unstable";
+  #   sha256 = "?";
+  # }
