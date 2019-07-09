2bbef8b40463822c497bb1565f3c7952c27412a5
Cameron Kingsbury <camsbury7@gmail.com>
2019-07-08 23:25:59 -0400

    feat: forward and backward epub nav

--- cmacs/config/epub-conf.el
+++ cmacs/config/epub-conf.el
@@ -1,6 +1,16 @@
 (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
 (setq nov-text-width 80)
+;; make this thing monospaced
+
 (general-add-hook 'nov-mode-hook
                   'evil-mode)
+
+(general-def 'normal nov-mode-map
+ [remap empty-mode-leader] #'hydra-nov/body)
+
+(defhydra hydra-nov (:exit t)
+  "nov-mode"
+  ("h" #'nov-previous-document "back")
+  ("l" #'nov-next-document "next"))
 
 (provide 'epub-conf)
