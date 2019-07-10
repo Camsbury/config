d4a9aa21fb8e70c070575b769de2b79d03b35c83
Cameron Kingsbury <camsbury7@gmail.com>
2019-07-09 23:27:43 -0400

    feat: hack to avoid quitting the book randomly

--- emacs.d/config/pdf-conf.el
+++ emacs.d/config/pdf-conf.el
@@ -2,6 +2,7 @@
  :keymaps 'doc-view-mode-map
  [remap doc-view-scroll-up-or-next-page] #'hydra-leader/body
  [remap describe-mode] #'doc-view-previous-page
+ "k"                   #'doc-view-previous-page
  "l"                   #'doc-view-next-page
  "n"                   #'doc-view-search-next-match
  "p"                   #'doc-view-search-previous-match
