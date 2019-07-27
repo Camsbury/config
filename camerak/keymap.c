2f8c1d89447048f8e5ed5de41f44e7f3b96b79d2
Cameron Kingsbury <camsbury7@gmail.com>
2019-07-27 13:00:54 -0400

    feat: add easier window switching on the ergodox

--- camerak/keymap.c
+++ camerak/keymap.c
@@ -7,7 +7,7 @@
 
 
 #define CHROME_INSPECT_M LGUI(LSFT(KC_C))
-#define CHROME_INSPECT_L KC_LCTL(LSFT(KC_J))
+#define CHROME_INSPECT_L LCTL(LSFT(KC_J))
 
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
@@ -77,27 +77,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // NUMBER LAYER
   [2] = LAYOUT_ergodox(
       //LEFT HAND
-      CHROME_INSPECT_L, KC_F1,        KC_F2,        KC_F3,      KC_F4,          KC_F5,        CHROME_INSPECT_M,
-      KC_NO,            KC_NO,        KC_NO,        KC_NO,      KC_NO, KC_NO,   KC_NO,
-      KC_NO,            KC_1,         KC_2,         KC_3,       KC_4,           KC_5,
-      KC_LCTL,          KC_NO,        KC_NO,        KC_NO,      KC_NO,          KC_NO,        KC_NO,
-      KC_NO,            ALL_T(KC_NO), MEH_T(KC_NO), KC_LALT,    KC_LGUI,
+      CHROME_INSPECT_L, KC_F1,              KC_F2,              KC_F3,            KC_F4,                KC_F5,              CHROME_INSPECT_M,
+      KC_NO,            LALT(KC_1),         LALT(KC_2),         LALT(KC_3),       LALT(KC_4),           LALT(KC_5),         KC_NO,
+      KC_NO,            KC_1,               KC_2,               KC_3,             KC_4,                 KC_5,
+      KC_LCTL,          LSFT(LALT(KC_1)),   LSFT(LALT(KC_2)),   LSFT(LALT(KC_3)), LSFT(LALT(KC_4)),     LSFT(LALT(KC_5)),   KC_NO,
+      KC_NO,            ALL_T(KC_NO),       MEH_T(KC_NO),       KC_LALT,          KC_LGUI,
 
-                                                                KC_NO,          KC_NO,
-                                                                                              KC_NO,
-                                                                KC_NO,          KC_TRNS,      KC_NO,
+                                                                                  KC_NO,                KC_NO,
+                                                                                                                            KC_NO,
+                                                                                  KC_NO,                KC_TRNS,            KC_NO,
 
 
       //RIGHT HAND
-      KC_NO,            KC_F6,        KC_F7,        KC_F8,      KC_F9,          KC_F10,       KC_F11,
-      KC_NO,            KC_NO,        KC_NO,        KC_NO,      KC_NO,          KC_NO,        KC_F12,
-                        KC_6,         KC_7,         KC_8,       KC_9,           KC_0,         KC_NO,
-      KC_NO,            KC_NO,        KC_NO,        KC_NO,      KC_NO,          KC_NO,        KC_LCTL,
-                                      KC_LGUI,      KC_LALT,    MEH_T(KC_NO),   ALL_T(KC_NO), KC_NO,
+      KC_NO,            KC_F6,              KC_F7,              KC_F8,            KC_F9,                KC_F10,             KC_F11,
+      KC_NO,            LALT(KC_6),         LALT(KC_7),         LALT(KC_8),       LALT(KC_9),           LALT(KC_0),         KC_F12,
+                        KC_6,               KC_7,               KC_8,             KC_9,                 KC_0,               KC_NO,
+      KC_NO,            LSFT(LALT(KC_6)),   LSFT(LALT(KC_7)),   LSFT(LALT(KC_8)), LSFT(LALT(KC_9)),     LSFT(LALT(KC_0)),   KC_LCTL,
+                                            KC_LGUI,            KC_LALT,          MEH_T(KC_NO),         ALL_T(KC_NO),       KC_NO,
 
       KC_NO,
-                        KC_NO,        KC_NO,
-      KC_NO,            KC_TRNS,      KC_NO),
+                        KC_NO,              KC_NO,
+      KC_NO,            KC_TRNS,            KC_NO),
 
   /* // BRAID LAYER */
   [3] = LAYOUT_ergodox(
