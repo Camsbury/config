f32ba0309bb8b1ee05a9f77a0f4eda2f6040fa6c
Cameron Kingsbury <camsbury7@gmail.com>
2019-08-23 18:24:18 -0400

    fix: update networking nix template

--- networking.example.nix
+++ networking.example.nix
@@ -6,7 +6,9 @@
   # define your hostname
   networking.hostName = "newMachine";
   # enables wireless support via wpa_supplicant
-  networking.wireless.enable = true;
+  # networking.wireless.enable = true;
+  # enables wireless support via NetworkManager
+  networking.networkmanager.enable = true;
 
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.enable = false;
