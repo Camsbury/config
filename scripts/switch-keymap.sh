c35bb4fc57af8798650ada49759dbdce621b8b99
Cameron Kingsbury <camsbury7@gmail.com>
2019-08-26 10:13:45 -0400

    feat: swap alt and super

--- scripts/switch-keymap.sh
+++ scripts/switch-keymap.sh
@@ -2,7 +2,7 @@
 variant=$(setxkbmap -query | grep variant)
 if [[ $variant == "" ]]
 then
-  setxkbmap -layout "us" -variant "colemak" -option "caps:escape"
+  setxkbmap -layout "us" -variant "colemak" -option "caps:escape" -option "altwin:swap_lalt_lwin"
 else
   setxkbmap -variant "" -option ""
 fi
