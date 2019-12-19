3a26aa2e39c7a442dbf71fad8585c51b7f79d74a
Cameron Kingsbury <camsbury7@gmail.com>
2019-12-18 19:27:21 -0500

    fix: manually start SLIME

--- emacs.d/config/clisp-conf.el
+++ emacs.d/config/clisp-conf.el
@@ -5,14 +5,12 @@
 (setq inferior-lisp-program "nix-shell -p sbcl --run sbcl")
 
 
-(general-add-hook 'lisp-mode-hook
-                  (list 'slime))
-
 (general-def 'normal lisp-mode-map
  [remap empty-mode-leader] #'hydra-clisp/body)
 
 (defhydra hydra-clisp (:exit t)
   "clisp-mode"
+  ("s" #'slime "start slime")
   ("E" #'slime-eval-buffer "eval buffer"))
 
 (nmap :states 'normal :keymaps 'lisp-mode-map
