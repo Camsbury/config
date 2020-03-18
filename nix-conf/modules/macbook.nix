51e47d19c7822b1dabb1726695b54699434db7e2
Cameron Kingsbury <camsbury7@gmail.com>
2020-03-18 09:43:08 -0400

    fix: reasonable resolution for nox

--- nix-conf/modules/macbook.nix
+++ nix-conf/modules/macbook.nix
@@ -2,4 +2,11 @@
 
 {
   boot.loader.grub.efiInstallAsRemovable = true;
+  services.xserver.xrandrHeads = [{
+    output = "eDP-1";
+    primary = true;
+    monitorConfig = ''
+      DisplaySize 406 228
+    '';
+  }];
 }
