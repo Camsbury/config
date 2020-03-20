5c401f8e392e86aa2b3c4d31323bacd194f35ede
Cameron Kingsbury <camsbury7@gmail.com>
2020-03-20 15:22:33 -0400

    fix: macbook keys left alone

--- nix-conf/modules/macbook.nix
+++ nix-conf/modules/macbook.nix
@@ -1,12 +1,31 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
-{
-  boot.loader.grub.efiInstallAsRemovable = true;
-  services.xserver.xrandrHeads = [{
-    output = "eDP-1";
-    primary = true;
-    monitorConfig = ''
-      DisplaySize 406 228
-    '';
-  }];
-}
+let
+  kernelPackages = config.boot.kernelPackages;
+in
+  {
+    imports = [
+      "${import ../utils/hardware.nix}/apple"
+    ];
+
+    services = {
+      xserver = {
+        xkbVariant = "colemak";
+        xkbOptions = "caps:escape";
+        xrandrHeads = [{
+          output = "eDP-1";
+          primary = true;
+          monitorConfig = ''
+            DisplaySize 406 228
+          '';
+        }];
+      };
+
+      udev.extraRules =
+        # Disable XHC1 wakeup signal to avoid resume getting triggered some time
+        # after suspend. Reboot required for this to take effect.
+        lib.optionalString
+          (lib.versionAtLeast kernelPackages.kernel.version "3.13")
+          ''SUBSYSTEM=="pci", KERNEL=="0000:00:14.0", ATTR{power/wakeup}="disabled"'';
+    };
+  }
