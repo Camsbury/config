d79ac4c9fe3ced760423c0b1efbc7c0aca85b5b0
Cameron Kingsbury <camsbury7@gmail.com>
2020-05-29 15:25:33 -0400

    feat: add general python shell

---
+++ nix-conf/shells/py.nix
@@ -0,0 +1,24 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+  with pkgs;
+  let
+    myPython =
+      python3.withPackages (
+        pythonPackages: with pythonPackages;
+          [ isort
+            ipython
+            mypy
+            pyflakes
+            pylint
+            jedi
+            yapf
+          ]
+      );
+  in
+    mkShell {
+      name = "python-shell";
+      buildInputs = [
+        myPython
+      ];
+    }
