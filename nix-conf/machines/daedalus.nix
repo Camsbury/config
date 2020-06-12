ba2f69cdf0b4ea17fea3caa088d4f03054d13c90
Cameron Kingsbury <camsbury7@gmail.com>
2020-06-11 21:06:48 -0400

    fix: make daedalus readable

--- nix-conf/machines/daedalus.nix
+++ nix-conf/machines/daedalus.nix
@@ -9,4 +9,18 @@
     ../modules/ssd.nix
     ../modules/tract.nix
   ];
+
+  services.xserver.xrandrHeads = [
+    { output = "eDP-1";
+      primary = true;
+      monitorConfig = ''
+        DisplaySize 406 228
+      '';
+    }
+    { output = "DP-3";
+      monitorConfig = ''
+        DisplaySize 508 285
+      '';
+    }
+  ];
 }
