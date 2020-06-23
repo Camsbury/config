da5e468fe54e997f2a827b2d0d9b2a54a4cd20b4
Cameron Kingsbury <camsbury7@gmail.com>
2020-06-23 09:36:34 -0400

    feat: bring functor up to date

--- nix-conf/machines/functor.nix
+++ nix-conf/machines/functor.nix
@@ -9,4 +9,17 @@
     ../modules/ssd.nix
     ../modules/music.nix
   ];
+
+  networking.hostName = "functor";
+
+  nix.trustedUsers = [ "root" "monoid" ];
+
+  users.users.monoid = {
+    isNormalUser = true;
+    home = "/home/monoid";
+    extraGroups = ["wheel" "networkmanager" "docker"];
+    shell = pkgs.zsh;
+  };
+
+  home-manager.users.monoid = import ../modules/home.nix;
 }
