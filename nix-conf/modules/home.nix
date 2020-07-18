04ee06b8a8eaed9a32e0ac922e7e8cd84de0e945
Cameron Kingsbury <camsbury7@gmail.com>
2020-07-17 20:01:01 -0400

    fix: tweak home-manager

--- nix-conf/modules/home.nix
+++ nix-conf/modules/home.nix
@@ -1,17 +1,23 @@
-  { ... }:
-  {
-    programs.home-manager.enable = true;
-    home.file = {
-      ".Xresources".source = ../../Xresources;
-      ".gitconfig".source = ../../gitconfig;
-      ".gnupg/gpg-agent.conf".source = ../../gpg-agent.conf;
-      ".gnupg/gpg.conf".source = ../../gpg.conf;
-      ".offlineimap.py".source = ../../offlineimap.py;
-      ".offlineimaprc".source = ../../offlineimaprc;
-      ".scripts".source = ../../scripts;
-      ".shells".source = ../shells;
-      ".tmux.conf".source = ../../tmux.conf;
-      ".xmonad".source = ../../xmonad;
-      ".zshrc".source = ../../zshrc;
-    };
-  }
+{ pkgs, ... }:
+let
+  unstable = import ../utils/unstable.nix {};
+in
+{
+  nixpkgs.config.allowUnfree = true;
+  programs.home-manager.enable = true;
+
+  # home.packages = [ unstable.hello ];
+  home.file = {
+    ".Xresources".source = ../../Xresources;
+    ".gitconfig".source = ../../gitconfig;
+    ".gnupg/gpg-agent.conf".source = ../../gpg-agent.conf;
+    ".gnupg/gpg.conf".source = ../../gpg.conf;
+    ".offlineimap.py".source = ../../offlineimap.py;
+    ".offlineimaprc".source = ../../offlineimaprc;
+    ".scripts".source = ../../scripts;
+    ".shells".source = ../shells;
+    ".tmux.conf".source = ../../tmux.conf;
+    ".xmonad".source = ../../xmonad;
+    ".zshrc".source = ../../zshrc;
+  };
+}
