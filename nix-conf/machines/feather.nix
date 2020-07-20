ca5b1a11c476595ef150e4c41a0a11e0decd0853
Cameron Kingsbury <camsbury7@gmail.com>
2020-07-19 20:21:44 -0400

    feat: update feather

--- nix-conf/machines/feather.nix
+++ nix-conf/machines/feather.nix
@@ -9,4 +9,26 @@
     ../modules/ssd.nix
     ../modules/music.nix
   ];
+
+  boot.initrd.luks.devices.crypted.device = "/dev/disk/by-uuid/33edaf89-8028-4432-9489-2bedeedb73df";
+
+  networking.hostName = "feather";
+
+  nix.trustedUsers = [
+    "root"
+    "quill"
+  ];
+
+  users.users.quill = {
+    home = "/home/quill";
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+       "docker"
+    ];
+    shell = pkgs.zsh;
+  };
+  home-manager.users.quill = import ../modules/home.nix;
 }
+
--- nix-conf/modules/home.nix
+++ nix-conf/modules/home.nix
@@ -12,12 +12,12 @@ in
     ".gitconfig".source = ../../gitconfig;
     ".gnupg/gpg-agent.conf".source = ../../gpg-agent.conf;
     ".gnupg/gpg.conf".source = ../../gpg.conf;
+    ".helpers.zsh.inc".source = ../../helpers.zsh.inc;
     ".offlineimap.py".source = ../../offlineimap.py;
     ".offlineimaprc".source = ../../offlineimaprc;
     ".scripts".source = ../../scripts;
     ".shells".source = ../shells;
     ".tmux.conf".source = ../../tmux.conf;
-    ".xmonad".source = ../../xmonad;
     ".zshrc".source = ../../zshrc;
   };
 }
