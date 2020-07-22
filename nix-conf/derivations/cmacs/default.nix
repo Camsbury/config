e9bdfebe9589e0eff53f2e1cff32a8ed185cb090
Cameron Kingsbury <camsbury7@gmail.com>
2020-07-22 19:09:59 -0400

    feat: dynamic linking of emacs config

--- nix-conf/derivations/cmacs/default.nix
+++ nix-conf/derivations/cmacs/default.nix
@@ -1,13 +1,14 @@
 { config, pkgs, ... }:
 
 with pkgs;
+with builtins;
 let
   custom-emacs = emacsPackagesNg.emacsWithPackages (import ../../packages/emacs.nix);
   config-path = ../../../emacs-conf;
 in
 pkgs.writeShellScriptBin "cmacs" ''
-  CONFIG_PATH=${config-path} \
+  CONFIG_PATH=${toString config-path} \
   PATH="${custom-emacs}/bin/emacs:$PATH" \
-  EMACSLOADPATH="${custom-emacs.deps}/share/emacs/site-lisp:${config-path}:" \
+  EMACSLOADPATH="${custom-emacs.deps}/share/emacs/site-lisp:${toString config-path}:" \
   exec ${custom-emacs}/bin/emacs --debug-init --no-site-file --no-site-lisp $@
 ''
