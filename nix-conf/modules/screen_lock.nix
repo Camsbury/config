0ef085b35a7c4eac0ac1f4f72ae2efe777bdc599
Cameron Kingsbury <camsbury7@gmail.com>
2020-07-27 17:17:52 -0400

    fix: correct slock call

--- nix-conf/modules/screen_lock.nix
+++ nix-conf/modules/screen_lock.nix
@@ -1,10 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = [pkgs.slock];
   security.wrappers.slock.source = "${pkgs.slock.out}/bin/slock";
   programs.xss-lock = {
     enable = true;
-    lockerCommand = "slock";
+    lockerCommand = "${pkgs.slock}/bin/slock";
   };
 }
