b128e44dbaa7c61182222a534a37a1241b5f01ec
Cameron Kingsbury <camsbury7@gmail.com>
2020-07-29 12:38:30 -0400

    fix: prodigy hotkeys

--- emacs-conf/core/env.el
+++ emacs-conf/core/env.el
@@ -4,14 +4,14 @@
   :config (direnv-mode))
 (use-package prodigy
   :config
-  (general-def 'normal prodigy-mode-map
-    "s" #'prodigy-start
-    "r" #'prodigy-restart
-    "m" #'prodigy-mark
-    "M" #'prodigy-mark-all
-    "u" #'prodigy-unmark
-    "U" #'prodigy-unmark-all
-    "S" #'prodigy-stop))
+  (general-def prodigy-mode-map
+    [remap evil-substitute]        #'prodigy-start
+    [remap evil-replace]           #'prodigy-restart
+    [remap evil-set-marker]        #'prodigy-mark
+    [remap evil-window-middle]     #'prodigy-mark-all
+    [remap undo-tree-undo]         #'prodigy-unmark
+    [remap undo-tree-visualize]    #'prodigy-unmark-all
+    [remap evil-change-whole-line] #'prodigy-stop))
 
 
 (provide 'core/env)
