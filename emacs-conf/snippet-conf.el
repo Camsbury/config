ced7c278fab9cfd6c5213b18466c683fb842fd8a
Cameron Kingsbury <camsbury7@gmail.com>
2020-10-22 07:27:52 -0400

    clean: clean up snippets

--- emacs-conf/snippet-conf.el
+++ emacs-conf/snippet-conf.el
@@ -1,7 +1,9 @@
 (use-package yasnippet
   :config
   (setq yas-snippet-dirs
-        (append yas-snippet-dirs `(,(concat (getenv "CONFIG_PATH") "/snippets/"))))
+        (append yas-snippet-dirs `(,(concat (getenv "CONFIG_PATH") "/snippets/")))
+        yas-snippet-dirs
+        (append yas-snippet-dirs `(,(concat (getenv "HOME") "/Dropbox/lxndr/snippets/"))))
   (yas-global-mode 1)
   (setq yas-triggers-in-field t))
 
