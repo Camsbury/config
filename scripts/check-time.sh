f3ddf12d8201996456a19f48254b50bcb68e2875
Cameron Kingsbury <camsbury7@gmail.com>
2020-11-15 12:38:42 -0500

    refactor: check-time is more intelligent

--- scripts/check-time.sh
+++ scripts/check-time.sh
@@ -1,3 +1,3 @@
 # check the time
-time=`timedatectl | grep "Local" | sed -r "s/^\s*(\S+\s+){4}(\S+)\s+\S+$/\2/"`
+time=`timedatectl | grep "Local" | sed -r "s/.*time: (.*)/\1/"`
 notify-send "$time"
