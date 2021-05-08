7b8082a6451860bfa07469c74650e8cc827b69ad
Cameron Kingsbury <camsbury7@gmail.com>
2021-05-07 22:37:47 -0400

    feat: update hardware and add hermes

---
+++ nix-conf/machines/hermes.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../modules/core.nix
+    ../modules/intel.nix
+    ../modules/laptop.nix
+    ../modules/non-ergodox.nix
+    ../modules/ssd.nix
+    ../modules/tract.nix
+    "${import ../utils/hardware.nix}/dell/xps/13-9310"
+  ];
+
+  boot.initrd.luks.devices.crypted.device = "/dev/disk/by-uuid/559bcebb-ef43-4d84-8550-8b371bfb6aa6";
+
+  networking.hostName = "hermes";
+
+  nix.trustedUsers = [
+    "root"
+    "camsbury"
+  ];
+
+  users.users.camsbury = {
+    home = "/home/camsbury";
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "docker"
+    ];
+    isNormalUser = true;
+    shell = pkgs.zsh;
+  };
+
+  services.xserver.xrandrHeads = [
+    { output = "eDP-1";
+      primary = true;
+      monitorConfig = ''
+        DisplaySize 406 228
+      '';
+    }
+    { output = "DP-3";
+      monitorConfig = ''
+        DisplaySize 508 285
+      '';
+    }
+  ];
+
+  home-manager.users.camsbury = import ../modules/home.nix;
+}
--- nix-conf/utils/hardware.nix
+++ nix-conf/utils/hardware.nix
@@ -4,10 +4,6 @@ in
   fetchFromGitHub {
     owner = "NixOS";
     repo  = "nixos-hardware";
-    # rev    = "03db9669a6fc712e9537201d55639287eb606765";
-    # sha256 = "1g8kap5qzva58pcwn2xj1cs0k9w9mpbrlk5diaaarlgizp4l2x0z";
-    # rev    = "f4364f2457051b407899f7fc3bced4ac952644ff";
-    # sha256 = "1g8kap5qzva58pcwn2xj1cs0k9w9mpbrlk5diaaarlgizp4l2x0z";
-    rev    = "16336614d3fb785e9526d19e461dc57ef081e8c4";
-    sha256 = "1y9xsh5dmdb8jliikz2ps5j11i7jbwlygkmbz6asw31g2ghqrm0w";
+    rev    = "f7540d6c27704ec0fe56ecc8b2a9b663181850b0";
+    sha256 = "1rr470h5frly4a6wwpm1pmfgv57v5f7zwlc73sdx93ms7pc050lf";
   }
