4ec30cd6c6886b7cbb2c92e8d87a43972beae89c
Cameron Kingsbury <camsbury7@gmail.com>
2021-06-02 16:17:58 -0400

    feat: more prodigy hotkeys

--- emacs-conf/dev/process.el
+++ emacs-conf/dev/process.el
@@ -1,11 +1,13 @@
 (use-package prodigy
   :config
   (general-def prodigy-mode-map
+    [remap evil-goto-line]         #'prodigy-jump-magit
     [remap evil-substitute]        #'prodigy-start
     [remap evil-replace]           #'prodigy-restart
     [remap evil-set-marker]        #'prodigy-mark
     [remap evil-window-middle]     #'prodigy-mark-all
     [remap evil-undo]              #'prodigy-unmark
+    [remap evil-ret]               #'prodigy-display-process
     [remap undo-tree-visualize]    #'prodigy-unmark-all
     [remap evil-change-whole-line] #'prodigy-stop))
 
