73f07103c5738fe7b71e9ea41352275534bca858
Cameron Kingsbury <camsbury7@gmail.com>
2021-06-03 12:40:50 -0400

    fix: add `windows-fix-broken-workspace` to remedy dead frames

--- emacs-conf/core/desktop/windows.el
+++ emacs-conf/core/desktop/windows.el
@@ -59,6 +59,23 @@
 
 (use-package buffer-move)
 
+(defun windows-fix-broken-workspace (broken-workspace)
+  "Place a new frame in the given frame/index, without affecting other frames"
+  (interactive
+   (list
+    (exwm-workspace--prompt-for-workspace
+     "Pick broken workspace [+/-]: ")))
+  (let* ((current-index exwm-workspace-current-index)
+         (default-limit exwm-workspace-switch-create-limit)
+         (temp-limit (inc default-limit)))
+    (customize-set-variable 'exwm-workspace-switch-create-limit temp-limit)
+    (exwm-workspace-switch-create default-limit)
+    (exwm-workspace-switch-create current-index)
+    (exwm-workspace-swap
+     (exwm-workspace--workspace-from-frame-or-index broken-workspace)
+     (exwm-workspace--workspace-from-frame-or-index default-limit))
+    (customize-set-variable 'exwm-workspace-switch-create-limit default-limit)))
+
 (defun set-window-width (count)
   "Set the selected window's width."
   (adjust-window-trailing-edge (selected-window) (- count (window-width)) t))
