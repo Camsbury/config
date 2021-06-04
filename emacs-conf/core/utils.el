702c63e86187d4f604d2490d589e387579cc10d8
Cameron Kingsbury <camsbury7@gmail.com>
2021-06-04 09:41:51 -0400

    refactor: uuid using emacs

--- emacs-conf/core/utils.el
+++ emacs-conf/core/utils.el
@@ -1,11 +1,7 @@
 (defun random-uuid ()
   "Returns a random UUID V4"
   (interactive)
-  (-> "nix-shell --pure \\
-       -p babashka \\
-       --run 'bb \"(str (java.util.UUID/randomUUID))\"'"
-      (shell-command-to-string)
-      (kill-new)))
+  (kill-new (uuid-string)))
 
 (defun file-to-string (file-name)
   (with-temp-buffer
