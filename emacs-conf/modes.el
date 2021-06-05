d40b84d532c696205df12edc9ead4bd7aeff22c6
Cameron Kingsbury <camsbury7@gmail.com>
2021-06-05 08:30:17 -0400

    clean: make blind-mode reasonable

--- emacs-conf/modes.el
+++ emacs-conf/modes.el
@@ -1,3 +1,15 @@
 (require 'modes/blind-mode)
+(require 'modes/prettify-mode)
+(defun minor-mode-active-p (minor-mode)
+  "Check if the passed minor-mode is active"
+  (not
+   (null
+    (--filter
+     (eq it minor-mode)
+     (--filter
+      (and
+       (boundp it)
+       (symbol-value it))
+      minor-mode-list)))))
 
 (provide 'modes)
--- emacs-conf/modes/blind-mode.el
+++ emacs-conf/modes/blind-mode.el
@@ -1,19 +1,14 @@
-(setq blind-mode-on nil)
 (setq blind-mode-font-height 130)
 
 (define-minor-mode blind-mode
   "Make it so blind people can see"
   :lighter " blind"
   :global t
-  :after-hook (progn
-                (if blind-mode-on
-                    (progn
-                      (setq blind-mode-on nil)
-                      (set-face-attribute
-                       'default nil :height normal-font-height))
-                  (progn
-                    (setq blind-mode-on t)
-                    (set-face-attribute
-                     'default nil :height blind-mode-font-height)))))
+  :after-hook
+  (if (minor-mode-active-p 'blind-mode)
+      (set-face-attribute
+       'default nil :height blind-mode-font-height)
+    (set-face-attribute
+     'default nil :height normal-font-height)))
 
 (provide 'modes/blind-mode)
