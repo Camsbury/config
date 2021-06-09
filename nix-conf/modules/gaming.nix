ebe9ea7d7b4b21d8e8babf49a17ed16635cb5c66
Cameron Kingsbury <camsbury7@gmail.com>
2021-06-08 21:39:06 -0400

    wine for battle.net

--- nix-conf/modules/gaming.nix
+++ nix-conf/modules/gaming.nix
@@ -4,6 +4,14 @@
   environment.systemPackages = with pkgs; [
     nvtop
     steam
+    (wineWowPackages.full.override {
+      wineRelease = "staging";
+      mingwSupport = true;
+    })
+    (winetricks.override {
+      wine = wineWowPackages.staging;
+    })
+    xorg.xgamma
   ];
 
   hardware.opengl.driSupport32Bit = true;
