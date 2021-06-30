8d49e336fea6d5e9e5d002357d0a144aeedb435c
Cameron Kingsbury <camsbury7@gmail.com>
2021-06-30 10:16:10 -0400

    fix: set BROWSER

--- nix-conf/modules/apps.nix
+++ nix-conf/modules/apps.nix
@@ -1,23 +1,28 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    anki
-    audacity
-    brave
-    chromium
-    discord
-    firefox
-    gimp
-    okular
-    peek # screen recorder
-    pgn-extract # chess utils
-    scid-vs-pc #chess
-    signal-desktop
-    slack
-    spotify # non-free
-    tdesktop # telegram
-    transmission
-    vlc
-  ];
+  environment = {
+    variables = {
+      BROWSER = "brave-browser";
+    };
+    systemPackages = with pkgs; [
+      anki
+      audacity
+      brave
+      chromium
+      discord
+      firefox
+      gimp
+      okular
+      peek # screen recorder
+      pgn-extract # chess utils
+      scid-vs-pc #chess
+      signal-desktop
+      slack
+      spotify # non-free
+      tdesktop # telegram
+      transmission
+      vlc
+    ];
+  };
 }
