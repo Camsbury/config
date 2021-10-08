00cd7706569591ba229ff183cde7cf58f3740138
Cameron Kingsbury <camsbury7@gmail.com>
2021-10-08 00:12:52 -0400

    feat: upg vbox

--- nix-conf/pins/vbox.nix
+++ nix-conf/pins/vbox.nix
@@ -2,10 +2,8 @@ let
   inherit (import <nixpkgs> {}) fetchFromGitHub;
 in
   import (fetchFromGitHub {
-    # owner = "NixOS";
-    # my fork
-    owner = "Camsbury";
+    owner = "NixOS";
     repo  = "nixpkgs";
-    rev = "90b3800db9f633aaf25e09902a5866f7355f2548";
-    sha256 = "1vrhwlwc7x3f0xn6cgckrfp8vky0yws30d52hq3miy122d03gcc0";
+    rev = "31ffc50c571";
+    sha256 = "1gg87k49rmdylmzxjzmllng78qr6wmssnci05z1kij3715wkqc5j";
   })
