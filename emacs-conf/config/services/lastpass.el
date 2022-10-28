bbb81c16a5c7725e2016b3e6d2b6d8bf401a1a5c
Cameron Kingsbury <camsbury7@gmail.com>
2022-10-28 18:35:27 -0400

    feat: lastpass commands

--- emacs-conf/config/services/lastpass.el
+++ emacs-conf/config/services/lastpass.el
@@ -8,4 +8,34 @@
   (setq lastpass-trust-login t)
   (lastpass-auth-source-enable))
 
+(defun lastpass-apps ()
+  (-drop-last
+   1
+   (split-string (shell-command-to-string "lpass ls --format=%an") "\n")))
+
+(defun lastpass-copy-username ()
+  (interactive)
+  (ivy-read
+   "App: "
+   (lastpass-apps)
+   :action (lambda (x)
+             (kill-new
+              (s-chomp
+               (shell-command-to-string
+                (concat  "lpass show --user " x)))))))
+
+(defun lastpass-copy-password ()
+  (interactive)
+  (ivy-read
+   "App: "
+   (lastpass-apps)
+   :action (lambda (x)
+             (kill-new
+              (s-chomp
+               (shell-command-to-string
+                (concat  "lpass show --password " x)))))))
+
+
+
 (provide 'config/services/lastpass)
+
