6841f4022f8ea601b0d8e455f1a9dddfdae16ae5
Cameron Kingsbury <camsbury7@gmail.com>
2023-01-15 15:55:46 -0500

    update discord

--- nix-conf/pins/discord.nix
+++ nix-conf/pins/discord.nix
@@ -6,6 +6,8 @@ in
     repo  = "nixpkgs";
     # rev = "51bcdc4cdaa";
     # sha256 = "0zpf159nlpla6qgxfgb2m3c2v09fz8jilc21zwymm59qrq6hxscm";
-    rev = "72b1ec0a79b1"; # pin
-    sha256 = "05r9dp15q6n6wxp37d81x31j5m5qpwcj23y2pfvaybcrdayg4x8x";
+    # rev = "72b1ec0a79b1"; # pin
+    # sha256 = "05r9dp15q6n6wxp37d81x31j5m5qpwcj23y2pfvaybcrdayg4x8x";
+    rev = "a518c771485";
+    hash = "sha256-oz757DnJ1ITvwyTovuwG3l9cX6j9j6/DH9eH+cXFJmc=";
 })
