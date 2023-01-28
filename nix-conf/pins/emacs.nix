e9886423a933f6b5787cb60f8f7d6158413693ca
Cameron Kingsbury <camsbury7@gmail.com>
2023-01-28 07:39:42 -0500

    clean: get rid of emacs pin

--- nix-conf/pins/emacs.nix
+++
@@ -1,9 +0,0 @@
-let
-  inherit (import <nixpkgs> {}) fetchFromGitHub;
-in
-  import (fetchFromGitHub {
-    owner = "NixOS";
-    repo  = "nixpkgs";
-    rev = "ac169ec6371f0d835542db654a65e0f2feb07838";
-    sha256 = "0bwjyz15sr5f7z0niwls9127hikp2b6fggisysk0cnk3l6fa8abh";
-})
