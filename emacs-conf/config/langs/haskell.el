f3fd3f305bbca1b1ae97ce14ea65306efcda4f9a
Cameron Kingsbury <camsbury7@gmail.com>
2023-02-17 13:24:05 -0500

    lots of haskell updates!

--- emacs-conf/config/langs/haskell.el
+++ emacs-conf/config/langs/haskell.el
@@ -1,80 +1,57 @@
 (use-package haskell)
-(use-package haskell-mode)
+(use-package haskell-mode
+  :config
+  (setq haskell-compile-cabal-build-command
+        "cabal new-build --ghc-option=-ferror-spans")
+  (setq haskell-compile-cabal-build-alt-command
+        "cabal clean && cabal new-build --ghc-option=-ferror-spans"))
 (use-package haskell-font-lock
   :after (haskell-mode)
   :config
   (setq haskell-font-lock-symbols-alist
-        (-reject
-         (lambda (elem) (string-equal "()" (car elem)))
-         haskell-font-lock-symbols-alist)
-
-        haskell-font-lock-symbols-alist
-        (cons
-         '("^." . "⌾")
-         haskell-font-lock-symbols-alist)
-
-        haskell-font-lock-symbols-alist
-        (cons
-         '("^?" . "⍉")
-         haskell-font-lock-symbols-alist)
-
-        haskell-font-lock-symbols-alist
-        (cons
-         '(".~" . "≃")
-         haskell-font-lock-symbols-alist)
-
-        haskell-font-lock-symbols-alist
-        (cons
-         '(".=" . "≗")
-         haskell-font-lock-symbols-alist)
-
-        haskell-font-lock-symbols-alist
-        (cons
-         '("<>" . "⊕")
-         haskell-font-lock-symbols-alist)
-
-        haskell-font-lock-symbols-alist
-        (cons
-         '("->" . "⟶")
-         haskell-font-lock-symbols-alist)
-
-        haskell-font-lock-symbols-alist
-        (cons
-         '("<-" . "⟵")
-         haskell-font-lock-symbols-alist)
-
-        haskell-font-lock-symbols-alist
-        (cons
-         '("^.." . "⁞")
-         haskell-font-lock-symbols-alist)
-
-        haskell-font-lock-symbols-alist
-        (cons
-         '("<=<" . "↢")
-         haskell-font-lock-symbols-alist)
-
-        haskell-font-lock-symbols-alist
-        (cons
-         '(">=>" . "↣")
-         haskell-font-lock-symbols-alist)
-
-        haskell-font-lock-symbols-alist
-        (cons
-         '("=<<" . "⇤")
-         haskell-font-lock-symbols-alist)
-
-        haskell-font-lock-symbols-alist
-        (cons
-         '(">>=" . "⇥")
-         haskell-font-lock-symbols-alist)
-
+        '(("!!" . "‼")
+          ("&&" . "∧")
+          ("-<" . "↢")
+          ("->" . "→")
+          ("->" . "⟶")
+          ("." "∘" haskell-font-lock-dot-is-not-composition)
+          (".=" . "≗")
+          (".~" . "≃")
+          ("/=" . "≢")
+          ("::" . "∷")
+          ("<-" . "←")
+          ("<-" . "⟵")
+          ("<=" . "≤")
+          ("<=<" . "↢")
+          ("<>" . "⊕")
+          ("=<<" . "⇤")
+          ("==" . "≡")
+          ("=>" . "⇒")
+          (">=" . "≥")
+          (">=>" . "↣")
+          (">>=" . "⇥")
+          ("\\" . "λ")
+          ("^." . "⌾")
+          ("^.." . "⁞")
+          ("^?" . "⍉")
+          ("forall" . "∀")
+          ("not" . "¬")
+          ("pi" . "π")
+          ("sqrt" . "√")
+          ("undefined" . "⊥")
+          ("||" . "∨")
+          ("~>" . "⇝"))
         haskell-font-lock-symbols t
-
         haskell-process-use-presentation-mode t
-
         haskell-interactive-mode-scroll-to-bottom t
-
         haskell-process-type 'cabal-repl))
+
+(use-package lsp-haskell
+  :after lsp-mode
+  :custom
+  (lsp-haskell-server-path "haskell-language-server")
+  :config
+  (add-hook 'haskell-mode-hook #'lsp-deferred))
 
 (use-package hlint-refactor
   :after (haskell-mode)
@@ -92,37 +69,47 @@
 (use-package company-cabal
   :after (company))
 
-(use-package dante
-  :after (haskell-mode)
-  :commands 'dante-mode
-  :hook  (haskell-mode . dante-mode)
-  :init
-  (setq dante-tap-type-time 1)
-  :config
-  (general-add-hook 'dante-mode-hook
-                    (list
-                     '(lambda ()
-                        (flycheck-add-next-checker
-                         'haskell-dante
-                         '(info . haskell-hlint)))
-                     '(lambda ()
-                        (eldoc-mode -1)))))
+;; (use-package dante
+;;   :after (haskell-mode)
+;;   :commands 'dante-mode
+;;   :hook  (haskell-mode . dante-mode)
+;;   :init
+;;   (setq dante-tap-type-time 1)
+;;   :config
+;;   (general-add-hook 'dante-mode-hook
+;;                     (list
+;;                      '(lambda ()
+;;                         (flycheck-add-next-checker
+;;                          'lsp
+;;                          '(error . haskell-dante))
+;;                         (flycheck-add-next-checker
+;;                          'haskell-dante
+;;                          '(info . haskell-hlint)))
+;;                      '(lambda ()
+;;                         (eldoc-mode -1)))))
 
-(use-package attrap
-  :after (dante))
+;; (use-package attrap
+;;   :after (dante))
+
+
 
 (general-def 'normal haskell-mode-map
   [remap empty-mode-leader] #'hydra-haskell/body)
 
 (defhydra hydra-haskell (:exit t)
   "haskell-mode"
+ ("a" #'lsp-execute-code-action          "execute code action")
+ ("c" #'haskell-compile                  "compile!")
+ ("C" (lambda ()
+        (interactive)
+        (haskell-compile '-))             "clean and compile!")
+ ("d" #'lsp-doc-show                     "show docs")
  ("e" #'haskell-align-imports            "align imports")
  ("s" #'haskell-sort-imports             "sort imports")
- ("i" #'dante-info                       "dante info")
- ("l" #'haskell-process-load-file        "load file")
- ("r" #'dante-restart                    "dante restart")
+ ("l" #'lsp-lens-mode                    "toggle lenses")
+ ("r" #'lsp-restart-workspace            "restart lsp workspace")
  ("t" #'hlint-refactor-refactor-at-point "hlint point")
  ("T" #'hlint-refactor-refactor-buffer   "hlint buffer")
- ("y" #'dante-type-at                    "dante type"))
+ )
 
 (provide 'config/langs/haskell)
