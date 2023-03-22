f1c014bdaba5a34941ad86b1eb23e6087b7756e8
Cameron Kingsbury <camsbury7@gmail.com>
2023-03-22 11:33:16 -0400

    add calibre

--- nix-conf/modules/utils.nix
+++ nix-conf/modules/utils.nix
@@ -5,6 +5,7 @@
   ];
   environment.systemPackages = with pkgs; [
     cachix
+    calibre # ebook stuff
     ghostscript # for viewing pdfs
     gnuplot
     mpg123 # used in emacs and other quick mp3 playing
