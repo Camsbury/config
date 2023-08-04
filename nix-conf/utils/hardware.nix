06772362228876867d28469d72a8ba34991159d5
Cameron Kingsbury <camsbury7@gmail.com>
2023-08-04 11:08:22 -0400

    sha updates

--- nix-conf/utils/hardware.nix
+++ nix-conf/utils/hardware.nix
@@ -11,6 +11,9 @@ in
     # NOTE: flip
     # rev = "c3c66f6db4ac74a59eb83d83e40c10046ebc0b8c";
     # sha256 = "1h5x8zgmxzdj15pgssn7nihi24ni63571q75hdpsz7zxgyjw2nyh";
-    rev = "feceb4d24f582817d8f6e737cd40af9e162dee05";
-    hash = "sha256-h8e3+5EZFbYHTMb0DN2ACuQTJBNHpqigvmEV1w2WIuE=";
+    # rev = "feceb4d24f582817d8f6e737cd40af9e162dee05";
+    # hash = "sha256-h8e3+5EZFbYHTMb0DN2ACuQTJBNHpqigvmEV1w2WIuE=";
+
+    rev = "429f232fe1dc398c5afea19a51aad6931ee0fb89";
+    hash = "sha256-aqKCUD126dRlVSKV6vWuDCitfjFrZlkwNuvj5LtjRRU=";
   }
--- nix-conf/utils/unstable.nix
+++ nix-conf/utils/unstable.nix
@@ -6,6 +6,10 @@ in
     repo  = "nixpkgs";
     # current nixos channel set at:
     # https://github.com/NixOS/nixpkgs/archive/4d2b37a8.tar.gz
-    rev = "03fb7220163";
-    hash = "sha256-Y/uC2ZmkQkyrdRZ5szZilhZ/46786Wio5CGTgL+Vb/c=";
+    # rev = "03fb7220163";
+    # hash = "sha256-Y/uC2ZmkQkyrdRZ5szZilhZ/46786Wio5CGTgL+Vb/c=";
+    # rev = "fe2ecaf706a";
+    # hash = "sha256-JEdQHsYuCfRL2PICHlOiH/2ue3DwoxUX7DJ6zZxZXFk=";
+	  rev = "7cc30fd5372d";
+    hash = "sha256-gBlBtk+KrezFkfMrZw6uwTuA7YWtbFciiS14mEoTCo0=";
 })
