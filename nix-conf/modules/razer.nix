1646a2496fe31715579238b2e76c435a881cdae3
Cameron Kingsbury <camsbury7@gmail.com>
2023-08-07 14:39:43 -0400

    automatically set razer sensitivity

--- nix-conf/modules/razer.nix
+++ nix-conf/modules/razer.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
 {
+  # Assumes Razer is device 9
+  services.xserver.displayManager.sessionCommands = ''
+    ${pkgs.xorg.xinput}/bin/xinput --set-prop \
+    9 "Device Accel Constant Deceleration" 0.4
+  '';
   hardware.openrazer.enable = true;
   users.extraGroups.openrazer.members = [
     "${toString config.users.users.default.name}"
