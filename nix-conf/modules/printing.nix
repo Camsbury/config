1c2bc3199bef183be6a7035c898d70dbe5a35df2
Cameron Kingsbury <camsbury7@gmail.com>
2023-08-11 10:54:51 -0400

    fix printing

--- nix-conf/modules/printing.nix
+++ nix-conf/modules/printing.nix
@@ -1,8 +1,17 @@
 { config, pkgs, ... }:
 
 {
-  services.printing.enable = true;
-  services.avahi.enable = true;
-  services.avahi.nssmdns = true;
-  services.avahi.openFirewall = true;
+  services.printing = {
+    enable = true;
+    drivers = [pkgs.hplipWithPlugin];
+  };
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    openFirewall = true;
+  };
+  # Be sure to add printers as root!
+  environment.systemPackages = [
+    pkgs.system-config-printer
+  ];
 }
