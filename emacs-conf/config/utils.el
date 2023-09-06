71fcc9e826005aba23142cc33c22cb786b7d7b86
Cameron Kingsbury <camsbury7@gmail.com>
2023-09-05 21:48:26 -0400

    delete file and buffer

--- emacs-conf/config/utils.el
+++ emacs-conf/config/utils.el
@@ -8,4 +8,16 @@
     (insert-file-contents file-name)
     (buffer-string)))
 
+(defun delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if filename
+        (if (y-or-n-p (concat "Do you really want to delete file " filename " ?"))
+            (progn
+              (delete-file filename)
+              (message "Deleted file %s." filename)
+              (kill-buffer)))
+      (message "Not a file visiting buffer!"))))
+
 (provide 'config/utils)
