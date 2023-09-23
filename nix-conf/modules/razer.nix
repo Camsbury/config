35f1ba169b72676b0ca50087157a9962b3da25d8
Cameron Kingsbury <camsbury7@gmail.com>
2023-09-23 12:45:41 -0400

    disable razer side keys

--- nix-conf/modules/razer.nix
+++ nix-conf/modules/razer.nix
@@ -3,6 +3,8 @@
 {
   # Assumes Razer is device 9
   services.xserver.displayManager.sessionCommands = ''
+    ${pkgs.xorg.xinput}/bin/xinput set-button-map \
+    9 1 2 3 4 5 6 7 0 0 && \
     ${pkgs.xorg.xinput}/bin/xinput --set-prop \
     9 "Device Accel Constant Deceleration" 0.4
   '';
