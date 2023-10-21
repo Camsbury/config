0fb9f58850a455386ccbfa9d9ec3aa22971aad35
Cameron Kingsbury <camsbury7@gmail.com>
2023-10-21 12:42:36 -0400

    update mouse settings for libinput

--- nix-conf/modules/razer.nix
+++ nix-conf/modules/razer.nix
@@ -6,7 +6,7 @@
     ${pkgs.xorg.xinput}/bin/xinput set-button-map \
     9 1 2 3 4 5 6 7 0 0 && \
     ${pkgs.xorg.xinput}/bin/xinput --set-prop \
-    9 "Device Accel Constant Deceleration" 0.4
+    9 "libinput Accel Speed" 1
   '';
   hardware.openrazer.enable = true;
   users.extraGroups.openrazer.members = [
--- nix-conf/modules/slimblade.nix
+++ nix-conf/modules/slimblade.nix
@@ -5,6 +5,6 @@
     ${pkgs.xorg.xinput}/bin/xinput set-button-map \
     "Kensington Slimblade Trackball" 1 2 3 4 5 0 0 3 && \
     ${pkgs.xorg.xinput}/bin/xinput --set-prop \
-    "Kensington Slimblade Trackball" "Device Accel Constant Deceleration" 0.55
+    "Kensington Slimblade Trackball" "libinput Accel Speed" 1
   '';
 }
