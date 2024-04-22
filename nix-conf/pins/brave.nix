e67b48b5ab6f0385f550212f585cee72ca66f344
Cameron Kingsbury <camsbury7@gmail.com>
2024-04-22 09:39:01 -0400

    update brave

--- nix-conf/pins/brave.nix
+++ nix-conf/pins/brave.nix
@@ -4,6 +4,6 @@ in
   import (fetchFromGitHub {
     owner = "NixOS";
     repo  = "nixpkgs";
-    rev = "b50a77c03d64";
-    hash = "sha256-zJaF0RkdIPbh8LTmnpW/E7tZYpqIE+MePzlWwUNob4c=";
+    rev = "d6b3ddd253c5";
+    hash = "sha256-kR7C7Fqt3JP40h0mzmSZeWI5pk1iwqj4CSeGjnUbVHc=";
 })
