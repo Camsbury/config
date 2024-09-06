3e64d073574517ea412842875d73790312b808c1
Cameron Kingsbury <camsbury7@gmail.com>
2024-09-06 15:03:29 -0400

    warcraft 3 hero/item config

--- camerak/keymap.c
+++ camerak/keymap.c
@@ -170,9 +170,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [6] = LAYOUT_ergodox(
       //LEFT HAND
       KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-      KC_NO,  KC_NO, KC_NO, KC_P7, KC_P8, KC_P9, KC_NO,
-      KC_DEL, KC_NO, KC_NO, KC_P4, KC_P5, KC_P6,
-      KC_INS, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_NO,
+      KC_NO,  KC_NO, KC_P1, KC_P4, KC_P7, KC_NO, KC_NO,
+      KC_DEL, KC_NO, KC_F3, KC_F2, KC_F1, KC_NO,
+      KC_INS, KC_NO, KC_P2, KC_P5, KC_P8, KC_NO, KC_NO,
       KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,
 
                                    KC_NO, KC_NO,
