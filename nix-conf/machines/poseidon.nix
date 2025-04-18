080b7f6a1671d63964189c721bdec12661273a27
Cameron Kingsbury <camsbury7@gmail.com>
2025-04-17 21:58:22 -0400

    adding hard drives

--- nix-conf/machines/poseidon.nix
+++ nix-conf/machines/poseidon.nix
@@ -30,6 +30,9 @@
   ];
 
   boot.initrd.luks.devices.crypted.device = "/dev/disk/by-uuid/a5f95eb4-a033-40c9-81a1-4ae489adfc7c";
+  boot.initrd.luks.devices.cryptedStore.device = "/dev/disk/by-uuid/77a45769-1398-44bd-a7a4-ebb05bfad2f6";
+  boot.initrd.luks.devices.cryptedSSD500G.device = "/dev/disk/by-uuid/88df2045-baed-444d-ad6f-3832d841ee61";
+  boot.initrd.luks.devices.cryptedHDD16T.device = "/dev/disk/by-uuid/720ce7b5-e3aa-4b7e-a079-e06c9c3e42a0";
 
   networking.hostName = "poseidon";
   users.users.default.name = "camsbury";
