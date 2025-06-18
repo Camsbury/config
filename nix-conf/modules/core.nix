a3121a7ed5d6fcb4adc0bfd133e931c1053621aa
Cameron Kingsbury <camsbury7@gmail.com>
2025-06-17 20:11:08 -0400

    feat: add cuda-maintainers cachix

--- nix-conf/modules/core.nix
+++ nix-conf/modules/core.nix
@@ -50,14 +50,17 @@
       substituters = [
         "https://cache.iog.io"
         "https://nix-community.cachix.org"
+        "https://cuda-maintainers.cachix.org"
       ];
       trusted-substituters = [
         "https://cache.iog.io"
         "https://nix-community.cachix.org"
+        "https://cuda-maintainers.cachix.org"
       ];
       trusted-public-keys = [
         "cache.iog.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       ];
     };
     nixPath = [
