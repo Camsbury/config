a51dd6f2b87a3e1fb93a03650051f315dfa719e6
Cameron Kingsbury <camsbury7@gmail.com>
2025-08-13 13:22:57 -0400

    fix: get rid of audio alerts for stupid stuff

--- nix-conf/modules/audio.nix
+++ nix-conf/modules/audio.nix
@@ -11,18 +11,25 @@
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
+    extraConfig.pipewire = {
+      "99-disable-bell" = {
+        "context.properties"= {
+          "module.x11.bell" = false;
+        };
+      };
+    };
     pulse.enable = true;
-  };
-
-  services.pipewire.wireplumber = {
-    enable = true;
-    extraConfig.bluetoothEnhancements = {
-      "monitor.bluez.properties" = {
-        "bluez5.enable-sbc-xq" = true;
-        "bluez5.enable-msbc" = true;
-        "bluez5.enable-hw-volume" = true;
-        "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+    wireplumber = {
+      enable = true;
+      extraConfig.bluetoothEnhancements = {
+        "monitor.bluez.properties" = {
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-hw-volume" = true;
+          "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+        };
       };
     };
   };
+
 }
