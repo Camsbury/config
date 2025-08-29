8b5a5aada147380a35acffaf554eabe03c8489c4
Cameron Kingsbury <camsbury7@gmail.com>
2025-08-29 13:19:27 -0400

    fix: playing with graphics card settings

--- nix-conf/modules/rtx-5070-ti.nix
+++ nix-conf/modules/rtx-5070-ti.nix
@@ -2,16 +2,20 @@
 
 {
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    version = "570.153.02";
-    sha256_64bit = "sha256-FIiG5PaVdvqPpnFA5uXdblH5Cy7HSmXxp6czTfpd4bY=";
-    sha256_aarch64 = "sha256-FIiG5PaVdvqPpnFA5uXdblH5Cy7HSmXxp6czTfpd4bY=";
-    openSha256 = "sha256-2DpY3rgQjYFuPfTY4U/5TcrvNqsWWnsOSX0f2TfVgTs=";
-    settingsSha256 = "sha256-5m6caud68Owy4WNqxlIQPXgEmbTe4kZV2vZyTWHWe+M=";
-    persistencedSha256 = lib.fakeSha256;
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "570.153.02";
+      sha256_64bit = "sha256-FIiG5PaVdvqPpnFA5uXdblH5Cy7HSmXxp6czTfpd4bY=";
+      sha256_aarch64 = "sha256-FIiG5PaVdvqPpnFA5uXdblH5Cy7HSmXxp6czTfpd4bY=";
+      openSha256 = "sha256-2DpY3rgQjYFuPfTY4U/5TcrvNqsWWnsOSX0f2TfVgTs=";
+      settingsSha256 = "sha256-5m6caud68Owy4WNqxlIQPXgEmbTe4kZV2vZyTWHWe+M=";
+      persistencedSha256 = lib.fakeSha256;
+    };
+    open = true;
+    modesetting.enable = true;
+    forceFullCompositionPipeline = true;
   };
-  hardware.nvidia.open = true;
-  hardware.nvidia.modesetting.enable = true;
 
   services.ollama.acceleration = "cuda";
+
 }
