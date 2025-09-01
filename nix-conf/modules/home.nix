c11605e225e895020b3905ba95af24523cb09cb4
Cameron Kingsbury <camsbury7@gmail.com>
2025-09-01 19:18:07 -0400

    fix: general gtk stuff

--- nix-conf/modules/home.nix
+++ nix-conf/modules/home.nix
@@ -8,8 +8,6 @@ in
 
     xsession.numlock.enable = true;
 
-    home.stateVersion = "22.05";
-
     dconf = {
       enable = true;
       settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
@@ -26,34 +24,30 @@ in
       };
 
       gtk3.extraConfig = {
-        Settings = ''
-          gtk-application-prefer-dark-theme=1
-        '';
-      };
-
-      gtk4.extraConfig = {
-        Settings = ''
-          gtk-application-prefer-dark-theme=1
-        '';
+        gtk-application-prefer-dark-theme = "1";
       };
     };
 
-    home.file = {
-      ".Xresources".source = ../../Xresources;
-      ".clojure".source = sym ../../clojure;
-      ".config/dunst/dunstrc".source = sym ../../dunstrc;
-      ".config/gollama/config.json".source = sym ../../gollama-conf.json;
-      ".config/msmtp/config".source = ../../msmtp-config;
-      ".gitconfig".source = sym ../../gitconfig;
-      ".gitignore".source = sym ../../global-gitignore;
-      ".gnupg/gpg-agent.conf".source = ../../gpg-agent.conf;
-      ".gnupg/gpg.conf".source = ../../gpg.conf;
-      ".helpers.zsh.inc".source = ../../helpers.zsh.inc;
-      ".mbsyncrc".source = ../../mbsyncrc;
-      ".rgignore".source = sym ../../rgignore;
-      ".scripts".source = sym ../../scripts;
-      ".shells".source = ../shells;
-      ".tmux.conf".source = ../../tmux.conf;
-      ".zshrc".source = ../../zshrc;
+    home = {
+      stateVersion = "22.05";
+      file = {
+        ".Xresources".source = ../../Xresources;
+        ".clojure".source = sym ../../clojure;
+        ".config/dunst/dunstrc".source = sym ../../dunstrc;
+        ".config/gollama/config.json".source = sym ../../gollama-conf.json;
+        ".config/msmtp/config".source = ../../msmtp-config;
+        ".gitconfig".source = sym ../../gitconfig;
+        ".gitignore".source = sym ../../global-gitignore;
+        ".gnupg/gpg-agent.conf".source = ../../gpg-agent.conf;
+        ".gnupg/gpg.conf".source = ../../gpg.conf;
+        ".helpers.zsh.inc".source = ../../helpers.zsh.inc;
+        ".config/libvirt".source = sym ../../libvirt;
+        ".mbsyncrc".source = ../../mbsyncrc;
+        ".rgignore".source = sym ../../rgignore;
+        ".scripts".source = sym ../../scripts;
+        ".shells".source = ../shells;
+        ".tmux.conf".source = ../../tmux.conf;
+        ".zshrc".source = ../../zshrc;
+      };
     };
   }
