356b515499ce4a248f8a34f9247d16fcf05b3e2b
Cameron Kingsbury <camsbury7@gmail.com>
2025-11-08 21:09:39 -0500

    fix: make bluetooth audio work

--- nix-conf/modules/audio.nix
+++ nix-conf/modules/audio.nix
@@ -19,17 +19,20 @@
       };
     };
     pulse.enable = true;
+
     wireplumber = {
       enable = true;
-      extraConfig.bluetoothEnhancements = {
+      extraConfig = {
+        "wireplumber.settings" = {
+          "bluetooth.autoswitch-to-headset-profile" = false;
+        };
         "monitor.bluez.properties" = {
           "bluez5.enable-sbc-xq" = true;
           "bluez5.enable-msbc" = true;
           "bluez5.enable-hw-volume" = true;
-          "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+          "bluez5.roles" = [ "a2dp_sink" "a2dp_source" ];
         };
       };
     };
   };
-
 }
