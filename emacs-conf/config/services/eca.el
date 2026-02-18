3de972a034a2469636438c062d6c1ea842fda9e1
Cameron Kingsbury <camsbury7@gmail.com>
2026-02-18 09:57:12 -0500

    chore: eca stuff

--- emacs-conf/config/services/eca.el
+++ emacs-conf/config/services/eca.el
@@ -5,6 +5,7 @@
   :hook
   (eca-chat-mode . (lambda () (whitespace-mode -1)))
   :config
+  (setq eca-chat-use-side-window nil)
 
   (general-def 'normal eca-chat-mode-map
     [remap empty-mode-leader]     #'hydra-eca/body))
@@ -12,7 +13,7 @@
 (defhydra hydra-eca (:exit t :columns 5)
   "eca-chat-mode"
   ("c" #'eca-chat-clear "Clear the chat")
-  ("b" #'eca-chat-cycle-behavior "Cycle behavior")
+  ("b" #'eca-chat-select-behavior "Cycle behavior")
   ("a" #'eca-chat-tool-call-accept-all "Accept all chat tool calls")
   ("A" #'eca-chat-tool-call-accept-all-and-remember "Accept all chat tool calls (and remember)")
   ("m" #'eca-chat-select-model "Select the model"))
