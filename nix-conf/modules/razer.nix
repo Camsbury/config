e26af46c0d7963c0b12b5084b152f69a3849e097
Cameron Kingsbury <camsbury7@gmail.com>
2026-05-07 15:29:52 -0400

    fix: mouse configs set right

--- nix-conf/modules/razer.nix
+++ nix-conf/modules/razer.nix
@@ -2,12 +2,24 @@
 
 {
   # Assumes Razer is device 9
-  services.xserver.displayManager.sessionCommands = ''
-    ${pkgs.xorg.xinput}/bin/xinput set-button-map \
-    9 1 2 3 4 5 6 7 0 0 && \
-    ${pkgs.xorg.xinput}/bin/xinput --set-prop \
-    9 "libinput Accel Speed" 1
-  '';
+  # services.xserver.displayManager.sessionCommands = ''
+  #   ${pkgs.xorg.xinput}/bin/xinput set-button-map \
+  #   9 1 2 3 4 5 6 7 0 0 && \
+  #   ${pkgs.xorg.xinput}/bin/xinput --set-prop \
+  #   9 "libinput Accel Speed" 1
+  # '';
+  services.xserver.inputClassSections = [
+    ''
+    Identifier   "Razer Razer DeathAdder V2"
+    MatchProduct "Razer"
+    MatchUSBID     "1532:0084"
+    MatchIsPointer "on"
+    Driver       "libinput"
+    Option       "AccelProfile" "adaptive"
+    Option       "AccelSpeed"   "1.0"
+    Option       "ButtonMapping" "1 2 3 4 5 6 7 0 0"
+    ''
+  ];
   hardware.openrazer.enable = true;
   users.extraGroups.openrazer.members = [
     "${toString config.users.users.default.name}"
--- nix-conf/modules/slimblade.nix
+++ nix-conf/modules/slimblade.nix
@@ -1,10 +1,23 @@
 { config, pkgs, ... }:
 
 {
-  services.xserver.displayManager.sessionCommands = ''
-    ${pkgs.xorg.xinput}/bin/xinput set-button-map \
-    "Kensington Slimblade Trackball" 1 2 3 4 5 0 0 3 && \
-    ${pkgs.xorg.xinput}/bin/xinput --set-prop \
-    "Kensington Slimblade Trackball" "libinput Accel Speed" 1
-  '';
+  # services.xserver.displayManager.sessionCommands = ''
+  #   ${pkgs.xorg.xinput}/bin/xinput set-button-map \
+  #   "Kensington Slimblade Trackball" 1 2 3 4 5 0 0 3 && \
+  #   ${pkgs.xorg.xinput}/bin/xinput --set-prop \
+  #   "Kensington Slimblade Trackball" "libinput Accel Speed" 1
+  # '';
+
+  services.xserver.inputClassSections = [
+    ''
+    Identifier   "Kensington Slimblade Trackball"
+    MatchProduct "Kensington"
+    MatchIsPointer "on"
+    Driver       "libinput"
+    Option       "AccelProfile" "adaptive"
+    Option       "AccelSpeed"   "1.0"
+    Option       "TransformationMatrix" "2.5 0 0 0 2.5 0 0 0 1"
+    Option       "ButtonMapping" "1 2 3 4 5 0 0 3"
+    ''
+  ];
 }
