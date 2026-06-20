f676a32f03e9e574ad60b52486ab75e0157dcc4f
Cameron Kingsbury <camsbury7@gmail.com>
2026-06-19 22:45:46 -0400

    chore: remove dead pins (last-unstable, vbox)

--- nix-conf/pins.nix
+++ nix-conf/pins.nix
@@ -1,12 +1,6 @@
 let
-  # nixos channel pinned to 26.05 - https://github.com/NixOS/nixpkgs/archive/bd0ff2d3eac2.tar.gz
-
   # hash via
   # nix --extra-experimental-features nix-command store prefetch-file --unpack https://github.com/NixOS/nixpkgs/archive/<REV>.tar.gz
-  last-unstable = {
-    rev = "cadcc8de2476";
-    hash = "sha256-QJiih52NU+nm7XQWCj+K8SwUdIEayDQ1FQgjkYISt4I=";
-  };
   unstable = {
     rev = "4100e830e085";
     hash = "sha256-NOF9NAREhxr50bbBfVcVOq+ArCMSoe8dP79Pk2uyARk=";
@@ -36,6 +30,5 @@ in
   hardware = fetchHardware hardware;
   spotify = fetchPackages unstable;
   unstable = fetchPackages unstable;
-  vbox = fetchPackages unstable;
   wine = fetchPackages unstable;
 }
