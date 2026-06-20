dd601f2445c0f7d421e256c4b1a40385478d3304
Cameron Kingsbury <camsbury7@gmail.com>
2026-06-19 22:44:11 -0400

    chore: remove dead derivations, overlays, and patch
    Derivations: scalastyle, datomic-cli, datomic-socks-proxy, clj2nix
    (also had typo defult.nix), duka, disper (python2, consumer commented
    out). Removed disper from overlays/core.nix.
    
    Overlays: xps.nix (never imported). Removed commented-out exwm patch
    override from emacs.nix and its patch file.

--- nix-conf/overlays/core.nix
+++ nix-conf/overlays/core.nix
@@ -1,55 +1,55 @@
 { config, pkgs, ... }:
 
-
 let
   unstablePkgs = import (import ../pins.nix).unstable {
-    config = { allowUnfree = true; };
+    config = {
+      allowUnfree = true;
+    };
   };
   spotifyPkgs = import (import ../pins.nix).spotify {
-    config = { allowUnfree = true; };
+    config = {
+      allowUnfree = true;
+    };
   };
 
   # cachixBall = import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/tarball/1d4de0d552ae9aa66a5b8dee5fb0650a4372d148") {};
 in
-  {
-    nixpkgs.overlays = [
-      (self: super: {
+{
+  nixpkgs.overlays = [
+    (
+      self: super:
+      {
 
-        alias-tips = with builtins; with pkgs;
-          callPackage (import ../derivations/alias-tips) {};
+        alias-tips = with builtins; with pkgs; callPackage (import ../derivations/alias-tips) { };
 
         # babashka = with builtins; with pkgs;
         #   callPackage (import ../derivations/babashka) {};
 
-        check-low-battery = with builtins; with pkgs;
-          callPackage (import ../derivations/check-low-battery) {};
+        check-low-battery =
+          with builtins;
+          with pkgs;
+          callPackage (import ../derivations/check-low-battery) { };
 
         # cachix = cachixBall.cachix;
 
-        cmacs = with builtins; with pkgs;
-          callPackage (import ../derivations/cmacs) {};
+        cmacs = with builtins; with pkgs; callPackage (import ../derivations/cmacs) { };
 
-        cmacs-load-path = with builtins; with pkgs;
-          callPackage (import ../derivations/cmacs-load-path) {};
+        cmacs-load-path = with builtins; with pkgs; callPackage (import ../derivations/cmacs-load-path) { };
 
-        disper = with builtins; with pkgs;
-          callPackage (import ../derivations/disper) {};
+        gollama = with builtins; with pkgs; callPackage (import ../derivations/gollama) { };
 
-        gollama = with builtins; with pkgs;
-          callPackage (import ../derivations/gollama) {};
+        lmstudio = with builtins; with pkgs; callPackage (import ../derivations/lmstudio) { };
 
-        lmstudio = with builtins; with pkgs;
-          callPackage (import ../derivations/lmstudio) {};
-
-        pgn-extract = with builtins; with pkgs;
-          callPackage (import ../derivations/pgn-extract) {};
+        pgn-extract = with builtins; with pkgs; callPackage (import ../derivations/pgn-extract) { };
 
         spotify = spotifyPkgs.spotify;
 
-        xndr = super.callPackage (builtins.fetchTarball
-          "https://github.com/Camsbury/xndr/archive/094be18.tar.gz") {pkgs = self;};
+        xndr =
+          super.callPackage (builtins.fetchTarball "https://github.com/Camsbury/xndr/archive/094be18.tar.gz")
+            { pkgs = self; };
 
-      } // ( with unstablePkgs; {
+      }
+      // (with unstablePkgs; {
         inherit bat;
         # inherit dropbox;
         inherit emacs;
@@ -60,7 +60,7 @@ in
         inherit mu;
         inherit netdata;
       })
-      )
-      (import ./emacs.nix)
-    ];
-  }
+    )
+    (import ./emacs.nix)
+  ];
+}
--- nix-conf/overlays/emacs.nix
+++ nix-conf/overlays/emacs.nix
@@ -2,166 +2,158 @@ self: super:
 
 let
   basePkgs = import (import ../pins.nix).unstable {
-    config = {allowUnfree = true;};
+    config = {
+      allowUnfree = true;
+    };
   };
   # Would be sweet to use the ones from nixpkgs instead
   compileEmacsFiles = basePkgs.callPackage ./emacsBuilder.nix;
-  emacsOverrides = eSelf: eSuper:
-    {
-      melpaPackages = eSuper.melpaPackages // {
-        cider = eSelf.melpaBuild {
-          pname = "cider";
-          version = "1.18.0";
+  emacsOverrides = eSelf: eSuper: {
+    melpaPackages = eSuper.melpaPackages // {
+      cider = eSelf.melpaBuild {
+        pname = "cider";
+        version = "1.18.0";
+
+        recipe = builtins.toFile "recipe.el" ''
+          (cider :fetcher github
+                 :repo "clojure-emacs/cider"
+                 :files (;; new code layout:
+                         "lisp/*.el" "bin/*.sh"
+                         ;; old code layout - will be kept for a while during the transition:
+                         "*.el" "clojure.sh" "lein.sh" (:exclude ".dir-locals.el"))
+                 :old-names (nrepl))
+        '';
+        buildInputs = with eSelf.melpaPackages; [
+          clojure-mode
+          eSelf.elpaPackages.queue
+          eSelf.elpaPackages.seq
+          eSelf.elpaPackages.spinner
+          parseedn
+          sesman
+          transient
+        ];
+
+        src = super.fetchFromGitHub {
+          owner = "clojure-emacs";
+          repo = "cider";
+          rev = "v1.18.0";
+          hash = "sha256-qgFmyPGpjUhMbIFGMYBzrlmrKj+/EnszNwVe4FlhmWU=";
+        };
+      };
+
+      eca =
+        let
+          version = "20260618.103";
+          rev = "e472b5";
+          hash = "sha256-84BPKyM3iGA6bhDhxREp/nUnuEfMLrxgqu5WoPHaDAI=";
+        in
+        eSelf.melpaBuild {
+          pname = "eca";
+          version = version;
 
           recipe = builtins.toFile "recipe.el" ''
-            (cider :fetcher github
-                   :repo "clojure-emacs/cider"
-                   :files (;; new code layout:
-                           "lisp/*.el" "bin/*.sh"
-                           ;; old code layout - will be kept for a while during the transition:
-                           "*.el" "clojure.sh" "lein.sh" (:exclude ".dir-locals.el"))
-                   :old-names (nrepl))
-          '';
-          buildInputs = with eSelf.melpaPackages; [
-            clojure-mode
-            eSelf.elpaPackages.queue
-            eSelf.elpaPackages.seq
-            eSelf.elpaPackages.spinner
-            parseedn
-            sesman
-            transient
-          ];
-
-          src = super.fetchFromGitHub {
-            owner = "clojure-emacs";
-            repo = "cider";
-            rev = "v1.18.0";
-            hash = "sha256-qgFmyPGpjUhMbIFGMYBzrlmrKj+/EnszNwVe4FlhmWU=";
-          };
-        };
-
-        eca =
-          let
-            version = "20260618.103";
-            rev = "e472b5";
-            hash = "sha256-84BPKyM3iGA6bhDhxREp/nUnuEfMLrxgqu5WoPHaDAI=";
-          in
-            eSelf.melpaBuild {
-              pname = "eca";
-              version = version;
-
-
-              recipe = builtins.toFile "recipe.el" ''
             (eca :fetcher github :repo "editor-code-assistant/eca-emacs")
           '';
 
-              buildInputs = with eSelf.melpaPackages; [
-                dash
-                f
-                markdown-mode
-              ];
+          buildInputs = with eSelf.melpaPackages; [
+            dash
+            f
+            markdown-mode
+          ];
 
-              src = super.fetchFromGitHub {
-                owner = "editor-code-assistant";
-                repo = "eca-emacs";
-                rev = rev;
-                hash = hash;
-              };
-            };
-      };
-      # elpaDevelPackages = eSuper.elpaDevelPackages // {
-      #   exwm = eSuper.elpaDevelPackages.exwm.overrideAttrs (old: {
-      #     src = super.runCommand "exwm-${old.version}.tar" { } ''
-      #       tar xf ${old.src}
-      #       d=$(echo exwm-*/); d=''${d%/}
-      #       patch "$d/exwm-manage.el" < ${./exwm-floating-unmanage-crash.patch}
-      #       tar --sort=name --owner=0 --group=0 --numeric-owner --mtime='@1' \
-      #           -cf "$out" "$d"
-      #     '';
-      #   });
-      # };
-
-      asoc-el = compileEmacsFiles {
-        name = "asoc.el";
-        src = builtins.fetchurl {
-          url = https://raw.githubusercontent.com/troyp/asoc.el/4a3309a9f250656da6f4a9d34feedf4f5666b17a/asoc.el;
-          sha256 = "1fdynjy8xmx4a41982793z9329121k2bzigpm4vljx1yflq52v2b";
+          src = super.fetchFromGitHub {
+            owner = "editor-code-assistant";
+            repo = "eca-emacs";
+            rev = rev;
+            hash = hash;
+          };
         };
+    };
+
+    asoc-el = compileEmacsFiles {
+      name = "asoc.el";
+      src = builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/troyp/asoc.el/4a3309a9f250656da6f4a9d34feedf4f5666b17a/asoc.el";
+        sha256 = "1fdynjy8xmx4a41982793z9329121k2bzigpm4vljx1yflq52v2b";
       };
+    };
 
-      company-postgresql = compileEmacsFiles {
-        name = "company-postgresql.el";
-        src = builtins.fetchurl {
-          url = https://raw.githubusercontent.com/urbint/emacs-postgresql-interactive/0c26f9cb4a7784c1eac121c30c557a70bff9b85e/company-postgresql.el;
-          sha256 = "1znqdnz5dx2cil7pilpggl18jkalys0923k26iabqlab9apygi0z";
-        };
-        buildInputs = with eSelf.melpaPackages; [
-          dash
-          emacsql
-          s
-        ];
+    company-postgresql = compileEmacsFiles {
+      name = "company-postgresql.el";
+      src = builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/urbint/emacs-postgresql-interactive/0c26f9cb4a7784c1eac121c30c557a70bff9b85e/company-postgresql.el";
+        sha256 = "1znqdnz5dx2cil7pilpggl18jkalys0923k26iabqlab9apygi0z";
       };
+      buildInputs = with eSelf.melpaPackages; [
+        dash
+        emacsql
+        s
+      ];
+    };
 
-      magit-difftastic = compileEmacsFiles {
-        name = "magit-difftastic.el";
-        src = builtins.fetchurl {
-          url = https://raw.githubusercontent.com/rschmukler/magit-difftastic/1e2a1f60288341893a9d21d8a900739be9f34e40/magit-difftastic.el;
-          sha256 = "0zr0n9x2029f4f2x33kjs7r826zc1kz7iziq4ik58w1nj4247qxz";
-        };
-        buildInputs = with eSelf.melpaPackages; [
-          cond-let
-          difftastic
-          llama
-          magit
-          magit-section
-          transient
-          with-editor
-        ];
+    magit-difftastic = compileEmacsFiles {
+      name = "magit-difftastic.el";
+      src = builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/rschmukler/magit-difftastic/1e2a1f60288341893a9d21d8a900739be9f34e40/magit-difftastic.el";
+        sha256 = "0zr0n9x2029f4f2x33kjs7r826zc1kz7iziq4ik58w1nj4247qxz";
       };
+      buildInputs = with eSelf.melpaPackages; [
+        cond-let
+        difftastic
+        llama
+        magit
+        magit-section
+        transient
+        with-editor
+      ];
+    };
 
-      etymology-of-word = compileEmacsFiles {
-        name = "etymology-of-word.el";
-        src = builtins.fetchurl {
-          url = https://raw.githubusercontent.com/Camsbury/etymology-of-word/master/etymology-of-word.el;
-          sha256 = "09yk4qrk3k5ygdqlj3ksdqzxh5532ychs4msphqrw3nim5dxhklw";
-        };
-        buildInputs = with eSelf.melpaPackages; [
-          dash
-        ];
+    etymology-of-word = compileEmacsFiles {
+      name = "etymology-of-word.el";
+      src = builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/Camsbury/etymology-of-word/master/etymology-of-word.el";
+        sha256 = "09yk4qrk3k5ygdqlj3ksdqzxh5532ychs4msphqrw3nim5dxhklw";
       };
+      buildInputs = with eSelf.melpaPackages; [
+        dash
+      ];
+    };
 
-      explain-pause-mode = eSelf.melpaBuild {
-        pname = "explain-pause-mode";
-        version = "0.1";
+    explain-pause-mode = eSelf.melpaBuild {
+      pname = "explain-pause-mode";
+      version = "0.1";
 
-        recipe = builtins.toFile "recipe.el" ''
-          (explain-pause-mode :fetcher github
-                              :repo "lastquestion/explain-pause-mode")
-        '';
+      recipe = builtins.toFile "recipe.el" ''
+        (explain-pause-mode :fetcher github
+                            :repo "lastquestion/explain-pause-mode")
+      '';
 
-        src = super.fetchFromGitHub {
-          owner = "lastquestion";
-          repo = "explain-pause-mode";
-          rev = "35f7d780a9c164b5c502023746473b1de3857904";
-          sha256 = "0d9lwzqqwmz0n94i7959rj7m24265yf3825a5g8cd7fyzxznl1pc";
-        };
+      src = super.fetchFromGitHub {
+        owner = "lastquestion";
+        repo = "explain-pause-mode";
+        rev = "35f7d780a9c164b5c502023746473b1de3857904";
+        sha256 = "0d9lwzqqwmz0n94i7959rj7m24265yf3825a5g8cd7fyzxznl1pc";
       };
+    };
 
-      hide-comnt = compileEmacsFiles {
-        name = "hide-comnt.el";
-        src = builtins.fetchurl {
-          url = https://raw.githubusercontent.com/emacsmirror/emacswiki.org/601b51e25e758083e66fab433cf61d22713fed51/hide-comnt.el;
-          sha256 = "0v3wgl9r9w0qbvs1cxgl7am9hvpy6hyhvfbsjqix5n0zmdg68s4n";
-        };
+    hide-comnt = compileEmacsFiles {
+      name = "hide-comnt.el";
+      src = builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/emacsmirror/emacswiki.org/601b51e25e758083e66fab433cf61d22713fed51/hide-comnt.el";
+        sha256 = "0v3wgl9r9w0qbvs1cxgl7am9hvpy6hyhvfbsjqix5n0zmdg68s4n";
       };
+    };
 
-      re-jump = compileEmacsFiles {
-        name = "re-jump.el";
-        src = builtins.fetchurl {
-          url = https://raw.githubusercontent.com/oliyh/re-jump.el/443ddfa33dd2ae593cc0a013d16fff21f2afd925/re-jump.el;
-          sha256 = "003zvvdlx77ncjml09gayspsrwynyqvhaip3cgzvn2nd92fwh9wk";
-        };
-        buildInputs = with eSelf.melpaPackages; with eSelf.elpaPackages; [
+    re-jump = compileEmacsFiles {
+      name = "re-jump.el";
+      src = builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/oliyh/re-jump.el/443ddfa33dd2ae593cc0a013d16fff21f2afd925/re-jump.el";
+        sha256 = "003zvvdlx77ncjml09gayspsrwynyqvhaip3cgzvn2nd92fwh9wk";
+      };
+      buildInputs =
+        with eSelf.melpaPackages;
+        with eSelf.elpaPackages;
+        [
           cider
           clojure-mode
           parseclj
@@ -170,10 +162,10 @@ let
           sesman
           spinner
         ];
-      };
     };
+  };
 
 in
-  {
-    emacsPackages = basePkgs.emacsPackages.overrideScope emacsOverrides;
-  }
+{
+  emacsPackages = basePkgs.emacsPackages.overrideScope emacsOverrides;
+}
